a226e29980e8ec2b428ec5499f55c47e69912c26
Marc Lundgren <marc.lundgren@liferay.com>
2013-06-14 17:03:50 -0700

    LPS-35900 - utilize YUI Cache class

--- portal-web/docroot/html/js/liferay/language.js
+++ portal-web/docroot/html/js/liferay/language.js
@@ -1,43 +1,58 @@
 ;(function(A, Liferay) {
-	A.use('io-base', function() {
-		Liferay.Language = {
-			get: function(key, extraParams) {
-				var instance = this;
+	A.use(
+		'cache',
+		'iobase',
+		function() {
+			Liferay.Language = {
+				get: function(key, extraParams) {
+					var instance = this;
 
-				var url = themeDisplay.getPathContext() + '/language/' + themeDisplay.getLanguageId() + '/' + key + '/';
+					var url = themeDisplay.getPathContext() + '/language/' + themeDisplay.getLanguageId() + '/' + key + '/';
 
-				if (extraParams) {
-					if (typeof extraParams == 'string') {
-						url += extraParams;
-					}
-					else if (Liferay.Util.isArray(extraParams)) {
-						url += extraParams.join('/');
-					}
-				}
-
-				var value = instance._cache[url];
-
-				if (!value) {
-					A.io(
-						url,
-						{
-							on: {
-								complete: function(i, o) {
-									value = o.responseText;
-								}
-							},
-							sync: true,
-							type: 'GET'
+					if (extraParams) {
+						if (typeof extraParams == 'string') {
+							url += extraParams;
 						}
-					);
+						else if (Liferay.Util.isArray(extraParams)) {
+							url += extraParams.join('/');
+						}
+					}
 
-					instance._cache[url] = value;
-				}
+					var value = instance._cache.retrieve(url);
 
-				return value;
-			},
+					if (!value) {
+						A.io(
+							url,
+							{
+								on: {
+									complete: function(i, o) {
+										value = o.responseText;
+									}
+								},
+								sync: true,
+								type: 'GET'
+							}
+						);
 
-			_cache: {}
-		};
-	}
+						instance._cache.add(url, value);
+					}
+
+					return value;
+				},
+
+				_cache: new A.Cache(
+					{
+						max: 250,
+						on: {
+							complete: function(i, o) {
+								value = o.responseText;
+							}
+						},
+						sync: true,
+						type: 'GET'
+					}
+				)
+			};
+		}
+	);
 })(AUI(), Liferay);
