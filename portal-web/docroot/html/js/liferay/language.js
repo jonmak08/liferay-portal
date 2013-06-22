c55b45d3ea13e8266ba965e9aba6c98c9ccbc32c
Marc Lundgren <marc.lundgren@liferay.com>
2013-06-21 18:22:16 -0700

    LPS-35900 Sites Admin New Web Content CKEditor does not appear

--- portal-web/docroot/html/js/liferay/language.js
+++ portal-web/docroot/html/js/liferay/language.js
@@ -1,45 +1,46 @@
-Liferay.Language = {
-	get: function(key, extraParams) {
-		var instance = this;
+;(function(A, Liferay) {
+	A.use(
+		'iobase',
+		function() {
+			Liferay.Language = {
+				get: A.cached(
+					function(key, extraParams) {
+						var instance = this;
 
-		var url = themeDisplay.getPathContext() + '/language/' + themeDisplay.getLanguageId() + '/' + key + '/';
+						var url = themeDisplay.getPathContext() + '/language/' + themeDisplay.getLanguageId() + '/' + key + '/';
 
-		if (extraParams) {
-			if (typeof extraParams == 'string') {
-				url += extraParams;
-			}
-			else if (Liferay.Util.isArray(extraParams)) {
-				url += extraParams.join('/');
-			}
-		}
-
-		var value = instance._cache[url];
-
-		var urlWithAuthToken = url;
-
-		if (Liferay.authToken) {
-			urlWithAuthToken += '?p_auth=' + Liferay.authToken;
-		}
-
-		if (!value) {
-			AUI().use('io-base').io(
-				urlWithAuthToken,
-				{
-					on: {
-						complete: function(i, o) {
-							value = o.responseText;
+						if (extraParams) {
+							if (typeof extraParams == 'string') {
+								url += extraParams;
+							}
+							else if (Liferay.Util.isArray(extraParams)) {
+								url += extraParams.join('/');
+							}
 						}
-					},
-					sync: true,
-					type: 'GET'
-				}
-			);
 
-			instance._cache[url] = value;
+						var urlWithAuthToken = url;
+
+						if (Liferay.authToken) {
+							urlWithAuthToken += '?p_auth=' + Liferay.authToken;
+						}
+
+						A.io(
+							urlWithAuthToken,
+							{
+								on: {
+									complete: function(i, o) {
+										value = o.responseText;
+									}
+								},
+								sync: true,
+								type: 'GET'
+							}
+						);
+
+						return value;
+					}
+				)
+			};
 		}
-
-		return value;
-	},
-
-	_cache: {}
-};
+	);
+})(AUI(), Liferay);
