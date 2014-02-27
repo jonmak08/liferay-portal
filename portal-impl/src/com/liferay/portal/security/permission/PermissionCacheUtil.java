c9d9e022891bdc26dfcb04f292098be793f04c70
Brian Chan <brian.chan@liferay.com>
2014-02-27 15:41:41 -0800

    LPS-44236 Match PERMISSION_CHECKER_BAG_CACHE_NAME

--- portal-impl/src/com/liferay/portal/security/permission/PermissionCacheUtil.java
+++ portal-impl/src/com/liferay/portal/security/permission/PermissionCacheUtil.java
@@ -47,7 +47,7 @@ public class PermissionCacheUtil {
 	public static final String RESOURCE_BLOCK_IDS_BAG_CACHE_NAME =
 		PermissionCacheUtil.class.getName() + "_RESOURCE_BLOCK_IDS_BAG";
 
-	public static final String USER_PERMISSION_CHECKER_BAG_PORTAL_CACHE =
+	public static final String USER_PERMISSION_CHECKER_BAG_CACHE_NAME =
 		PermissionCacheUtil.class.getName() + "_USER_PERMISSION_CHECKER";
 
 	public static final String USER_ROLE_CACHE_NAME =
@@ -222,7 +222,7 @@ public class PermissionCacheUtil {
 			PropsValues.PERMISSIONS_OBJECT_BLOCKING_CACHE);
 	private static PortalCache<Long, UserPermissionCheckerBag>
 		_userPermissionCheckerBagPortalCache = MultiVMPoolUtil.getCache(
-			USER_PERMISSION_CHECKER_BAG_PORTAL_CACHE,
+			USER_PERMISSION_CHECKER_BAG_CACHE_NAME,
 			PropsValues.PERMISSIONS_OBJECT_BLOCKING_CACHE);
 	private static final PortalCache<String, Boolean> _userRolePortalCache =
 		MultiVMPoolUtil.getCache(
