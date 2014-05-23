e2165f173b3a3c184183e527af452b9901a8a538
Pei-Jung Lan <pei-jung.lan@liferay.com>
2014-05-23 16:34:10 -0700

    LPS-47108 Move portlet.js to module

--- portal-web/docroot/html/js/liferay/modules.js
+++ portal-web/docroot/html/js/liferay/modules.js
@@ -652,6 +652,14 @@
 							'json'
 						]
 					},
+					'liferay-portlet': {
+						path: 'portlet.js',
+						requires: [
+							'liferay-dependency',
+							'liferay-events',
+							'liferay-util'
+						]
+					},
 					'liferay-portlet-base': {
 						path: 'portlet_base.js',
 						requires: [
