bf92f53e14fb7598794cf8f2f3b78143f97604df
Pei-Jung Lan <pei-jung.lan@liferay.com>
2014-05-23 16:08:25 -0700

    LPS-47103 Move events.js to module

--- portal-web/docroot/html/js/liferay/modules.js
+++ portal-web/docroot/html/js/liferay/modules.js
@@ -365,6 +365,13 @@
 							'aui-base'
 						]
 					},
+					'liferay-events': {
+						path: 'events.js',
+						requires: [
+							'attribute',
+							'oop'
+						]
+					},
 					'liferay-form': {
 						path: 'form.js',
 						requires: [
