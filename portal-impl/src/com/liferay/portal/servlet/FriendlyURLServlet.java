406d74b9f83303e1030ecf27edba5d1aa49e5731
Minhchau Dang <minhchau.dang@liferay.com>
2014-12-09 14:17:09 -0800

    LPS-51028 Compute length without string concatenation

--- portal-impl/src/com/liferay/portal/servlet/FriendlyURLServlet.java
+++ portal-impl/src/com/liferay/portal/servlet/FriendlyURLServlet.java
@@ -186,7 +186,11 @@ public class FriendlyURLServlet extends HttpServlet {
 			requestURI = requestURI.substring(0, pos);
 		}
 
-		return requestURI.substring(_friendlyURLPathPrefix.length());
+		String pathProxy = PortalUtil.getPathProxy();
+
+		pos = _friendlyURLPathPrefix.length() - pathProxy.length();
+
+		return requestURI.substring(pos);
 	}
 
 	protected Object[] getRedirect(
