ede31083e598979d7de27d0186b5168a567b4cb3
Jonathan McCann <jonathan.mccann@liferay.com>
2015-01-20 08:27:28 -0800

    LPS-52750 Revert "LPS-19866 Source formatting"
    This reverts commit 2c699f192e3cdc52c9fe8d855b5f1bf770978069.

--- util-taglib/src/com/liferay/taglib/security/DoAsURLTag.java
+++ util-taglib/src/com/liferay/taglib/security/DoAsURLTag.java
@@ -62,7 +62,9 @@ public class DoAsURLTag extends TagSupport {
 
 			if (!layouts.isEmpty()) {
 				layout = layouts.get(0);
+			}
 
+			if (layout != null) {
 				doAsURL = layout.getFriendlyURL();
 			}
 
