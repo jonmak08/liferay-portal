dd0b416645aa645b4bcbe7d50f62feb8ace31ea1
Cleydyr de Albuquerque <cleydyr.albuquerque@liferay.com>
2015-01-22 20:07:04 -0300

    LPS-53521 Apply same idea as LPS-44052

--- portal-impl/src/com/liferay/portal/servlet/filters/aggregate/AggregateFilter.java
+++ portal-impl/src/com/liferay/portal/servlet/filters/aggregate/AggregateFilter.java
@@ -45,10 +45,9 @@ import com.liferay.portal.util.PropsValues;
 
 import java.io.File;
 import java.io.IOException;
-
+import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -269,14 +268,30 @@ public class AggregateFilter extends IgnoreModuleRequestFilter {
 					continue;
 				}
 
-				URLConnection urlConnection = resourceURL.openConnection();
+				URLConnection urlConnection = null;
 
-				if (urlConnection.getLastModified() >
-						cacheFile.lastModified()) {
+				try {
+					urlConnection = resourceURL.openConnection();
 
-					staleCache = true;
+					if (urlConnection.getLastModified() >
+							cacheFile.lastModified()) {
 
-					break;
+						staleCache = true;
+
+						break;
+					}
+				}
+				finally {
+					if (urlConnection != null) {
+						try {
+							InputStream inputStream = urlConnection.getInputStream();
+
+							inputStream.close();
+						}
+						catch (IOException ioe) {
+
+						}
+					}
 				}
 			}
 
