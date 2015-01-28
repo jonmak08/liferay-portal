489f8f8563314a55fb78c810ce8fcbbb86ff1312
Eduardo Garcia <eduardo.garcia@liferay.com>
2015-01-28 17:30:52 +0100

    LPS-52400 Comment

--- portal-impl/src/com/liferay/portal/lar/LayoutImporter.java
+++ portal-impl/src/com/liferay/portal/lar/LayoutImporter.java
@@ -1125,6 +1125,9 @@ public class LayoutImporter {
 
 				Layout layout = layouts.get(layoutId);
 
+				// Layout might have not been imported due to a controlled error
+				// See SitesImpl#addMergeFailFriendlyURLLayout
+
 				if (layout == null) {
 					continue;
 				}
