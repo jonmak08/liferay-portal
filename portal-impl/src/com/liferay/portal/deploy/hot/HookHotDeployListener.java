7bb15c82a19296f561956d7813a9c536e6be5e29
Laszlo Csontos <laszlo.csontos@liferay.com>
2015-01-30 14:13:40 +0100

    LPS-53052 Fix log readability bug - not related

--- portal-impl/src/com/liferay/portal/deploy/hot/HookHotDeployListener.java
+++ portal-impl/src/com/liferay/portal/deploy/hot/HookHotDeployListener.java
@@ -1339,7 +1339,9 @@ public class HookHotDeployListener
 				i++;
 			}
 
-			_log.debug(sb.toString());
+			Log log = SanitizerLogWrapper.allowCRLF(_log);
+
+			log.debug(sb.toString());
 		}
 
 		if (customJspGlobal && !_customJspBagsMap.isEmpty() &&
