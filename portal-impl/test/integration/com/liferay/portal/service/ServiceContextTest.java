f463facf1ff1357241a2535dcea139ea02586917
Jenny Chen <jenny.chen@liferay.com>
2015-02-09 13:39:22 -0800

    LPS-51815 Update Test for backport

--- portal-impl/test/integration/com/liferay/portal/service/ServiceContextTest.java
+++ portal-impl/test/integration/com/liferay/portal/service/ServiceContextTest.java
@@ -15,9 +15,9 @@
 package com.liferay.portal.service;
 
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
-import com.liferay.portal.kernel.test.AggregateTestRule;
-import com.liferay.portal.test.LiferayIntegrationTestRule;
-import com.liferay.portal.test.MainServletTestRule;
+import com.liferay.portal.kernel.test.ExecutionTestListeners;
+import com.liferay.portal.test.LiferayIntegrationJUnitTestRunner;
+import com.liferay.portal.test.MainServletExecutionTestListener;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -25,22 +25,17 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 
 import org.junit.Assert;
-import org.junit.ClassRule;
-import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import org.powermock.api.mockito.PowerMockito;
 
 /**
  * @author Michael C. Han
  */
+@ExecutionTestListeners(listeners = {MainServletExecutionTestListener.class})
+@RunWith(LiferayIntegrationJUnitTestRunner.class)
 public class ServiceContextTest extends PowerMockito {
-
-	@ClassRule
-	@Rule
-	public static final AggregateTestRule aggregateTestRule =
-		new AggregateTestRule(
-			new LiferayIntegrationTestRule(), MainServletTestRule.INSTANCE);
 
 	@Test
 	public void testJSONSerialization() {
