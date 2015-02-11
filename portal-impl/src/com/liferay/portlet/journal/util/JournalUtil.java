e8345c768f83711517582aadcd5a6cf626a3b176
Jorge Díaz <jorge.diaz@liferay.com>
2015-02-11 17:17:01 +0100

    LPS-53281 Add import of Locale

--- portal-impl/src/com/liferay/portlet/journal/util/JournalUtil.java
+++ portal-impl/src/com/liferay/portlet/journal/util/JournalUtil.java
@@ -101,6 +101,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Stack;
 import java.util.regex.Pattern;
