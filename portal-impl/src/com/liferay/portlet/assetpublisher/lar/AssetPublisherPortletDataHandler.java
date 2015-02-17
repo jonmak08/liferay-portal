d66044dd86b7d79cc0375905b3cc8b9920c232c1
Daniel Kocsis <daniel.kocsis@liferay.com>
2015-02-17 15:31:06 +0100

    LPS-53638 Export tag dependencies when export an AP portlet

--- portal-impl/src/com/liferay/portlet/assetpublisher/lar/AssetPublisherPortletDataHandler.java
+++ portal-impl/src/com/liferay/portlet/assetpublisher/lar/AssetPublisherPortletDataHandler.java
@@ -21,6 +21,7 @@ import com.liferay.portal.kernel.lar.ExportImportHelperUtil;
 import com.liferay.portal.kernel.lar.PortletDataContext;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
@@ -191,6 +192,19 @@ public class AssetPublisherPortletDataHandler
 					portletDataContext, portlet, portletPreferences,
 					"queryValues" + index, AssetCategory.class.getName(),
 					portletDataContext.getExportDataRootElement());
+			}
+			else if (name.startsWith("queryName") &&
+					 StringUtil.equalsIgnoreCase(value, "assetTags")) {
+
+				String[] assetTagNames = portletPreferences.getValues(
+					"queryValues" + name.substring(9), null);
+
+				if (ArrayUtil.isEmpty(assetTagNames)) {
+					continue;
+				}
+
+				portletDataContext.addAssetTags(
+					AssetPublisher.class.getName(), 0, assetTagNames);
 			}
 			else if (name.equals("scopeIds")) {
 				updateExportScopeIds(
