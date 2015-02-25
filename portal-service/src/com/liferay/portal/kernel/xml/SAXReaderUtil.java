793484189fc50534f764c7a7b2242811004c501a
Brian Chan <brian.chan@liferay.com>
2015-02-25 15:51:17 -0800

    LPS-50479 SF

--- portal-service/src/com/liferay/portal/kernel/xml/SAXReaderUtil.java
+++ portal-service/src/com/liferay/portal/kernel/xml/SAXReaderUtil.java
@@ -259,7 +259,9 @@ public class SAXReaderUtil {
 
 		StackTraceElement stackTraceElement = stackTraceElements[2];
 
-		if (!stackTraceElement.getMethodName().startsWith("read")) {
+		String methodName = stackTraceElement.getMethodName();
+
+		if (!methodName.startsWith("read")) {
 			return false;
 		}
 
@@ -275,8 +277,9 @@ public class SAXReaderUtil {
 			if (callerSignature.startsWith(whitelistSignature)) {
 				if (_log.isDebugEnabled()) {
 					_log.debug(
-						"Unsecure SAXReader allowed for " + callerSignature +
-							" based on '" + whitelistSignature + "' rule");
+						"Unsecure SAX reader allowed for " + callerSignature +
+							" based on the \"" + whitelistSignature +
+								"\" whitelist");
 				}
 
 				return true;
@@ -284,7 +287,7 @@ public class SAXReaderUtil {
 		}
 
 		if (_log.isDebugEnabled()) {
-			_log.debug("Unsecure SAXReader disallowed for " + callerSignature);
+			_log.debug("Unsecure SAX reader disallowed for " + callerSignature);
 		}
 
 		return false;
