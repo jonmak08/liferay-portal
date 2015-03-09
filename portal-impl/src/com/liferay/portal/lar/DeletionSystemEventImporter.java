63d6374fc60bfc3d0fb8e74eb9c5bae85eaac6a4
Máté Thurzó <mate.thurzo@liferay.com>
2015-03-09 14:39:23 +0100

    LPS-54054 Prepare the portlet data context properly for deletions before doing anything else

--- portal-impl/src/com/liferay/portal/lar/DeletionSystemEventImporter.java
+++ portal-impl/src/com/liferay/portal/lar/DeletionSystemEventImporter.java
@@ -16,6 +16,7 @@ package com.liferay.portal.lar;
 
 import com.liferay.portal.kernel.lar.ExportImportPathUtil;
 import com.liferay.portal.kernel.lar.PortletDataContext;
+import com.liferay.portal.kernel.lar.PortletDataHandler;
 import com.liferay.portal.kernel.lar.PortletDataHandlerKeys;
 import com.liferay.portal.kernel.lar.StagedModelDataHandlerUtil;
 import com.liferay.portal.kernel.lar.StagedModelType;
@@ -23,13 +24,22 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.xml.Document;
+import com.liferay.portal.kernel.xml.DocumentException;
 import com.liferay.portal.kernel.xml.Element;
 import com.liferay.portal.kernel.xml.ElementHandler;
 import com.liferay.portal.kernel.xml.ElementProcessor;
+import com.liferay.portal.kernel.xml.SAXReaderUtil;
+import com.liferay.portal.model.Portlet;
 import com.liferay.portal.security.xml.SecureXMLFactoryProviderUtil;
+import com.liferay.portal.service.PortletLocalServiceUtil;
 
 import java.io.StringReader;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.xml.sax.InputSource;
@@ -50,6 +60,8 @@ public class DeletionSystemEventImporter {
 
 			return;
 		}
+
+		initDeletionSystemEventStagedModelTypes(portletDataContext);
 
 		String xml = portletDataContext.getZipEntryAsString(
 			ExportImportPathUtil.getSourceRootPath(portletDataContext) +
@@ -75,6 +87,91 @@ public class DeletionSystemEventImporter {
 		xmlReader.setContentHandler(elementHandler);
 
 		xmlReader.parse(new InputSource(new StringReader(xml)));
+	}
+
+	protected void initDeletionSystemEventStagedModelTypes(
+		PortletDataContext portletDataContext) {
+
+		Element importDataRootElement =
+			portletDataContext.getImportDataRootElement();
+
+		Element portletsElement = null;
+
+		if (importDataRootElement != null) {
+			portletsElement = importDataRootElement.element("portlets");
+		}
+
+		List<Element> portletElements = Collections.emptyList();
+
+		if (portletsElement != null) {
+			portletElements = portletsElement.elements("portlet");
+		}
+		else {
+			Element element = importDataRootElement.element("portlet");
+
+			portletElements = new ArrayList<Element>();
+
+			portletElements.add(element);
+		}
+
+		for (Element portletElement : portletElements) {
+			String portletPath = portletElement.attributeValue("path");
+
+			Document portletDocument = null;
+
+			try {
+				portletDocument = SAXReaderUtil.read(
+					portletDataContext.getZipEntryAsString(portletPath));
+			}
+			catch (DocumentException de) {
+				continue;
+			}
+
+			portletElement = portletDocument.getRootElement();
+
+			Element portletDataElement = null;
+
+			if (portletElement != null) {
+				portletDataElement = portletElement.element("portlet-data");
+			}
+
+			String portletId = portletElement.attributeValue("portlet-id");
+
+			Portlet portlet = PortletLocalServiceUtil.getPortletById(portletId);
+
+			if (!portlet.isActive()) {
+				continue;
+			}
+
+			PortletDataHandler portletDataHandler =
+				portlet.getPortletDataHandlerInstance();
+
+			if (portletDataHandler == null) {
+				continue;
+			}
+
+			Map<String, Boolean> importPortletControlsMap =
+				Collections.emptyMap();
+
+			try {
+				importPortletControlsMap =
+					LayoutImporter.getImportPortletControlsMap(
+						portletDataContext.getCompanyId(), portletId,
+						portletDataContext.getParameterMap(),
+						portletDataElement,
+						portletDataContext.getManifestSummary());
+			}
+			catch (Exception e) {
+			}
+
+			if (importPortletControlsMap.get(
+					PortletDataHandlerKeys.PORTLET_DATA)) {
+
+				portletDataContext.addDeletionSystemEventStagedModelTypes(
+					portletDataHandler.
+						getDeletionSystemEventStagedModelTypes());
+			}
+		}
 	}
 
 	protected void doImportDeletionSystemEvents(
