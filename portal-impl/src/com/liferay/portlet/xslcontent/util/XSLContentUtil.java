45fb2453fa34723da9082493d78b7fe40041e1ba
Jenny Chen <jenny.chen@liferay.com>
2015-03-13 15:08:01 -0700

    LPS-51026 SF

--- portal-impl/src/com/liferay/portlet/xslcontent/util/XSLContentUtil.java
+++ portal-impl/src/com/liferay/portlet/xslcontent/util/XSLContentUtil.java
@@ -20,6 +20,7 @@ import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.PortalUtil;
 import com.liferay.portal.util.PropsValues;
 
 import java.io.ByteArrayInputStream;
@@ -58,13 +59,14 @@ public class XSLContentUtil {
 
 		Bundle bundle = FrameworkUtil.getBundle(XSLContentUtil.class);
 
-		StringBundler sb = new StringBundler(3);
+		String symbolicName = bundle.getSymbolicName();
+
+		StringBundler sb = new StringBundler(4);
 
 		sb.append(themeDisplay.getPortalURL());
-		sb.append("/o/");
-		sb.append(
-			StringUtil.replace(
-				bundle.getSymbolicName(), StringPool.PERIOD, StringPool.BLANK));
+		sb.append(PortalUtil.getPathModule());
+		sb.append(StringPool.SLASH);
+		sb.append(symbolicName.replaceAll("[^a-zA-Z0-9]", StringPool.BLANK));
 
 		String portletContextUrl = sb.toString();
 
