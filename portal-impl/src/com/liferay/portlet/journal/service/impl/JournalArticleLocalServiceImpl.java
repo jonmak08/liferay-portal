e88567e9ab66c9745703e1b0c8bb8df4c995747d
Jonathan McCann <jonathan.mccann@liferay.com>
2015-03-19 08:49:51 -0700

    LPS-54376 Use constant

--- portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -5809,7 +5809,7 @@ public class JournalArticleLocalServiceImpl
 			String elName = imageEl.attributeValue("name");
 			String elIndex = imageEl.attributeValue("index");
 
-			String name = elName + "_" + elIndex;
+			String name = elName + StringPool.UNDERLINE + elIndex;
 
 			List<Element> dynamicContentEls = imageEl.elements(
 				"dynamic-content");
@@ -5863,7 +5863,7 @@ public class JournalArticleLocalServiceImpl
 				String elIndex = element.attributeValue(
 					"index", StringPool.BLANK);
 
-				String name = elName + "_" + elIndex;
+				String name = elName + StringPool.UNDERLINE + elIndex;
 
 				formatImage(
 					groupId, articleId, version, incrementVersion, element,
