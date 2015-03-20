ede7f24f66f7a348333c469bfd8cbe8ef44a5baf
Zoltán Takács <zoltan.takacs@liferay.com>
2015-03-20 10:58:38 +0100

    Revert "LPS-54396 This is so wasteful, if it needs to call get eventually, why fetch first!"
    This reverts commit 93f5d54bee48a9697e98873f7f72200d2720bcb0.

--- portal-impl/src/com/liferay/portlet/PortletPreferencesFactoryImpl.java
+++ portal-impl/src/com/liferay/portlet/PortletPreferencesFactoryImpl.java
@@ -121,6 +121,14 @@ public class PortletPreferencesFactoryImpl
 			themeDisplay.getScopeGroupId(), themeDisplay.getUserId(), layout,
 			portletId, false);
 
+		PortletPreferences portletPreferences =
+			PortletPreferencesLocalServiceUtil.fetchPreferences(
+				portletPreferencesIds);
+
+		if (portletPreferences != null) {
+			return;
+		}
+
 		PortletPreferencesLocalServiceUtil.getPreferences(
 			portletPreferencesIds);
 	}
