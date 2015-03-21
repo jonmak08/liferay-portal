1ec30ed3b7920b7f941b34f7b8397edd687b16f2
shuyangzhou <shuyang.zhou@liferay.com>
2015-03-20 20:18:52 -0700

    LPS-54449 Reset ActionResponse on action phase error (PLT.5.4.7, xxiii)

--- portal-impl/src/com/liferay/portlet/InvokerPortletImpl.java
+++ portal-impl/src/com/liferay/portlet/InvokerPortletImpl.java
@@ -308,6 +308,14 @@ public class InvokerPortletImpl implements InvokerPortlet {
 			invokeAction(actionRequest, actionResponse);
 		}
 		catch (PortletException pe) {
+
+			// PLT.5.4.7, TCK xxiii
+
+			StateAwareResponseImpl stateAwareResponseImpl =
+				(StateAwareResponseImpl)actionResponse;
+
+			stateAwareResponseImpl.reset();
+
 			actionRequest.setAttribute(
 				_portletId + PortletException.class.getName(), pe);
 		}
