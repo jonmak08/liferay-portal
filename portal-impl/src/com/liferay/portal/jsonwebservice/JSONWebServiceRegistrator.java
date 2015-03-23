cb7d336e1bf1d45034a87de7c149ca36eb61ebe4
riccardo-ferrari <riccardo.ferrari@liferay.com>
2015-03-23 16:25:50 +0100

    LPS-54465 JSONWebServiceRegistrator should skip null beans

--- portal-impl/src/com/liferay/portal/jsonwebservice/JSONWebServiceRegistrator.java
+++ portal-impl/src/com/liferay/portal/jsonwebservice/JSONWebServiceRegistrator.java
@@ -85,6 +85,10 @@ public class JSONWebServiceRegistrator {
 			return;
 		}
 
+		if (bean == null) {
+			return;
+		}
+
 		JSONWebService jsonWebService = AnnotationLocator.locate(
 			bean.getClass(), JSONWebService.class);
 
