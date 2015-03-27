7179344b14f8a7e39ac6ef35f8464e9c31afdce7
Dustin Ryerson <dustin.ryerson@liferay.com>
2015-03-27 12:30:41 -0700

    LPS-52824 Add new portal property to select ckeditor version

--- portal-service/src/com/liferay/portal/kernel/util/PropsKeys.java
+++ portal-service/src/com/liferay/portal/kernel/util/PropsKeys.java
@@ -835,6 +835,8 @@ public interface PropsKeys {
 
 	public static final String DYNAMIC_RESOURCE_SERVLET_ALLOWED_PATHS = "dynamic.resource.servlet.allowed.paths";
 
+	public static final String EDITOR_CKEDITOR_VERSION = "editor.ckeditor.version";
+
 	public static final String EDITOR_INLINE_EDITING_ENABLED = "editor.inline.editing.enabled";
 
 	public static final String EDITOR_WYSIWYG_DEFAULT = "editor.wysiwyg.default";
