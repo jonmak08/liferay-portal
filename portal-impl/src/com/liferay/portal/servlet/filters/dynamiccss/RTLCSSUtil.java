4a6ec9badc5dd4d12921fa04f6df356d7dc6a750
Eduardo Garcia <eduardo.garcia@liferay.com>
2015-03-27 15:05:39 +0100

    LPS-44291 Allow excluding paths from rtl generation

--- portal-impl/src/com/liferay/portal/servlet/filters/dynamiccss/RTLCSSUtil.java
+++ portal-impl/src/com/liferay/portal/servlet/filters/dynamiccss/RTLCSSUtil.java
@@ -18,6 +18,10 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.util.ClassLoaderUtil;
+import com.liferay.portal.util.PropsValues;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.Function;
@@ -71,7 +75,34 @@ public class RTLCSSUtil {
 		}
 	}
 
+	public static boolean isExcludedPath(String filePath) {
+		for (Pattern pattern : _RTL_CSS_EXCLUDED_PATHS_PATTERNS) {
+			Matcher matcher = pattern.matcher(filePath);
+
+			if (matcher.matches()) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
 	private static Log _log = LogFactoryUtil.getLog(RTLCSSUtil.class);
+
+	private static Pattern[] _RTL_CSS_EXCLUDED_PATHS_PATTERNS;
+
+	static {
+		String[] rtlCssExcludedPathsRegexp =
+			PropsValues.RTL_CSS_EXCLUDED_PATHS_REGEXP;
+
+		_RTL_CSS_EXCLUDED_PATHS_PATTERNS =
+			new Pattern[rtlCssExcludedPathsRegexp.length];
+
+		for (int i = 0; i < rtlCssExcludedPathsRegexp.length; i++) {
+			_RTL_CSS_EXCLUDED_PATHS_PATTERNS[i] = Pattern.compile(
+				rtlCssExcludedPathsRegexp[i]);
+		}
+	}
 
 	private static String _jsScript;
 
