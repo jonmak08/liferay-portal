302748933bb2003a3bef36d7cda044b27226cca8
Jonathan McCann <jonathan.mccann@liferay.com>
2015-04-02 14:11:57 -0700

    LPS-50382 Only company admin can see private key

---
+++ portal-impl/src/com/liferay/portal/json/transformer/CompanyJSONTransformer.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.json.transformer;
+
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.model.Company;
+import com.liferay.portal.security.permission.PermissionChecker;
+import com.liferay.portal.security.permission.PermissionThreadLocal;
+
+/**
+ * @author Igor Spasic
+ */
+public class CompanyJSONTransformer extends FlexjsonObjectJSONTransformer {
+
+	@Override
+	public void transform(Object object) {
+		Company company = (Company)object;
+
+		boolean hidePrivateData = true;
+
+		PermissionChecker permissionChecker =
+			PermissionThreadLocal.getPermissionChecker();
+
+		if (permissionChecker != null) {
+			if (permissionChecker.isCompanyAdmin(company.getCompanyId())) {
+				hidePrivateData = false;
+			}
+		}
+
+		if (hidePrivateData) {
+			company.setKey(StringPool.BLANK);
+			company.setKeyObj(null);
+		}
+
+		super.transform(object);
+	}
+
+}
