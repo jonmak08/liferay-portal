95aa92634da7678bb24b4dc7b01f856f13d70ed6
Hudak Laszlo <laszlo.hudak@liferay.com>
2015-04-13 12:59:03 +0200

    LPS-54728 Transform content upon displaying it according to the structure's localizability

--- portal-impl/src/com/liferay/portlet/journal/util/LocaleTransformerListener.java
+++ portal-impl/src/com/liferay/portlet/journal/util/LocaleTransformerListener.java
@@ -14,15 +14,23 @@
 
 package com.liferay.portlet.journal.util;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.templateparser.BaseTransformerListener;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.xml.Document;
 import com.liferay.portal.kernel.xml.Element;
 import com.liferay.portal.kernel.xml.SAXReaderUtil;
+import com.liferay.portal.service.ClassNameLocalServiceUtil;
+import com.liferay.portlet.dynamicdatamapping.model.DDMStructure;
+import com.liferay.portlet.dynamicdatamapping.service.DDMStructureLocalServiceUtil;
 import com.liferay.portlet.dynamicdatamapping.util.DDMXMLUtil;
+import com.liferay.portlet.journal.model.JournalArticle;
 
 import java.util.List;
 import java.util.Map;
@@ -51,6 +59,8 @@ public class LocaleTransformerListener extends BaseTransformerListener {
 		if (_log.isDebugEnabled()) {
 			_log.debug("onXml");
 		}
+
+		xml = filterByLocalizability(xml, tokens);
 
 		return filterByLanguage(xml, languageId);
 	}
@@ -142,6 +152,99 @@ public class LocaleTransformerListener extends BaseTransformerListener {
 		}
 
 		return xml;
+	}
+
+	protected String filterByLocalizability(
+		String xml, Map<String, String> tokens) {
+
+		if (xml == null || tokens == null) {
+			return xml;
+		}
+
+		try {
+			Document document = SAXReaderUtil.read(xml);
+
+			String structureKey = tokens.get("structure_id");
+
+			long groupId = Long.parseLong(tokens.get("article_group_id"));
+
+			DDMStructure structure = null;
+
+			structure = DDMStructureLocalServiceUtil.fetchStructure(
+					groupId, ClassNameLocalServiceUtil
+						.getClassNameId(JournalArticle.class), structureKey,
+					true);
+
+			if (Validator.isNull(structure)) {
+				return xml;
+			}
+
+			Element rootElement = document.getRootElement();
+
+			String defaultLanguageId = LocaleUtil.toLanguageId(
+				LocaleUtil.getSiteDefault());
+
+			String defaultLocale = rootElement.attributeValue(
+				"default-locale", defaultLanguageId);
+
+			filterByLocalizability(rootElement, defaultLocale, structure);
+
+			xml = DDMXMLUtil.formatXML(document);
+		}
+		catch (Exception e) {
+			_log.error(e);
+		}
+
+		return xml;
+	}
+
+	protected void filterByLocalizability(
+			Element root, String defaultLanguageId, DDMStructure structure)
+		throws PortalException, SystemException {
+
+		List<Element> elements = root.elements("dynamic-element");
+
+		int listIndex = elements.size() - 1;
+
+		while (listIndex >= 0) {
+			Element element = elements.get(listIndex);
+
+			String name = element.attributeValue("name");
+
+			if (!structure.hasField(name)) {
+				listIndex--;
+				continue;
+			}
+
+			if (!structure.isFieldTransient(name)) {
+				filterFields(element, structure, name, defaultLanguageId);
+			}
+
+			filterByLocalizability(element, defaultLanguageId, structure);
+
+			listIndex--;
+		}
+	}
+
+	protected void filterFields(
+			Element dynamicElementElement, DDMStructure ddmStructure,
+			String name, String defaultLanguageId)
+		throws PortalException, SystemException {
+
+		boolean localizable = GetterUtil.getBoolean(
+			ddmStructure.getFieldProperty(name, "localizable"));
+
+		List<Element> dynamicContentElements = dynamicElementElement.elements(
+			"dynamic-content");
+
+		for (Element dynamicContentElement : dynamicContentElements) {
+			String languageId = dynamicContentElement.attributeValue(
+				"language-id");
+
+			if (!localizable && !languageId.equals(defaultLanguageId)) {
+				dynamicElementElement.remove(dynamicContentElement);
+			}
+		}
 	}
 
 	private static Log _log = LogFactoryUtil.getLog(
