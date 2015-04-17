83c8a58aa34943108b87480e2dd28d6429ff7893
Michael Han <michael.han@liferay.com>
2015-04-17 12:42:23 -0500

    LPS-54965 exception already handled by ThrowableAwareRunnable

--- portal-impl/src/com/liferay/portal/verify/VerifyProcess.java
+++ portal-impl/src/com/liferay/portal/verify/VerifyProcess.java
@@ -85,20 +85,13 @@ public abstract class VerifyProcess extends BaseDBProcess {
 				throwableAwareRunnables)
 		throws Exception {
 
-		List<Throwable> throwables = new ArrayList<Throwable>();
-
 		if (throwableAwareRunnables.size() <
 				PropsValues.VERIFY_PROCESS_CONCURRENCY_THRESHOLD) {
 
 			for (ThrowableAwareRunnable throwableAwareRunnable :
 					throwableAwareRunnables) {
 
-				try {
-					throwableAwareRunnable.run();
-				}
-				catch (Exception e) {
-					throwables.add(e);
-				}
+				throwableAwareRunnable.run();
 			}
 		}
 		else {
@@ -116,18 +109,15 @@ public abstract class VerifyProcess extends BaseDBProcess {
 				List<Future<Object>> futures = executorService.invokeAll(jobs);
 
 				for (Future<Object> future : futures) {
-					try {
-						future.get();
-					}
-					catch (ExecutionException ee) {
-						throwables.add(ee.getCause());
-					}
+					future.get();
 				}
 			}
 			finally {
 				executorService.shutdown();
 			}
 		}
+
+		List<Throwable> throwables = new ArrayList<Throwable>();
 
 		for (ThrowableAwareRunnable throwableAwareRunnable :
 				throwableAwareRunnables) {
