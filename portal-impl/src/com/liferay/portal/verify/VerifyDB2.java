84f2be5193f0261e0c2faf5a590858be8b3cfd2c
jgok <joshua.gok@liferay.com>
2015-05-12 16:55:16 -0700

    LPS-55640 Add convertColumnToClob verify process for DB2

--- portal-impl/src/com/liferay/portal/verify/VerifyDB2.java
+++ portal-impl/src/com/liferay/portal/verify/VerifyDB2.java
@@ -28,6 +28,71 @@ import java.sql.ResultSet;
  */
 public class VerifyDB2 extends VerifyProcess {
 
+	protected void convertColumnToClob(String tableName, String columnName)
+		throws Exception {
+
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+			con = DataAccess.getUpgradeOptimizedConnection();
+
+			StringBundler sb = new StringBundler(6);
+
+			sb.append("select coltype from ");
+			sb.append("sysibm.syscolumns where tbname = '");
+			sb.append(tableName);
+			sb.append("' AND name = '");
+			sb.append(columnName);
+			sb.append("'");
+
+			ps = con.prepareStatement(sb.toString());
+
+			rs = ps.executeQuery();
+
+			if (rs.next()) {
+				String dataType = rs.getString(1);
+
+				if (dataType.equals("CLOB")) {
+					return;
+				}
+
+				runSQL("alter table " + tableName + " add temp CLOB");
+
+				sb = new StringBundler(4);
+
+				sb.append("update ");
+				sb.append(tableName);
+				sb.append(" set temp = ");
+				sb.append(columnName);
+
+				runSQL(sb.toString());
+
+				sb = new StringBundler(4);
+
+				sb.append("alter table ");
+				sb.append(tableName);
+				sb.append(" drop column ");
+				sb.append(columnName);
+
+				runSQL(sb.toString());
+
+				sb = new StringBundler(4);
+
+				sb.append("alter table ");
+				sb.append(tableName);
+				sb.append(" rename column temp to ");
+				sb.append(columnName);
+
+				runSQL(sb.toString());
+			}
+		}
+		finally {
+			DataAccess.cleanUp(con, ps, rs);
+		}
+	}
+
 	@Override
 	protected void doVerify() throws Exception {
 		DB db = DBFactoryUtil.getDB();
@@ -38,6 +103,11 @@ public class VerifyDB2 extends VerifyProcess {
 			return;
 		}
 
+		increaseColumnSize();
+		convertColumnToClob("EXPANDOVALUE", "DATA_");
+	}
+
+	protected void increaseColumnSize() throws Exception {
 		Connection con = null;
 		PreparedStatement ps = null;
 		ResultSet rs = null;
