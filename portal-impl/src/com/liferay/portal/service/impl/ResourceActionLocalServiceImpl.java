8640fd8e3e489ba95f3fd07fd6b2f8e6bc60b3a3
shuyangzhou <shuyang.zhou@liferay.com>
2015-06-08 14:48:55 -0700

    LPS-56195 SF

--- portal-impl/src/com/liferay/portal/service/impl/ResourceActionLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/ResourceActionLocalServiceImpl.java
@@ -179,20 +179,16 @@ public class ResourceActionLocalServiceImpl
 	public ResourceAction deleteResourceAction(long resourceActionId)
 		throws PortalException, SystemException {
 
-		ResourceAction resourceAction =
-			resourceActionPersistence.findByPrimaryKey(resourceActionId);
-
-		return deleteResourceAction(resourceAction);
+		return deleteResourceAction(
+			resourceActionPersistence.findByPrimaryKey(resourceActionId));
 	}
 
 	@Override
 	public ResourceAction deleteResourceAction(ResourceAction resourceAction)
 		throws SystemException {
 
-		String key = encodeKey(
-			resourceAction.getName(), resourceAction.getActionId());
-
-		_resourceActions.remove(key);
+		_resourceActions.remove(
+			encodeKey(resourceAction.getName(), resourceAction.getActionId()));
 
 		return resourceActionPersistence.remove(resourceAction);
 	}
