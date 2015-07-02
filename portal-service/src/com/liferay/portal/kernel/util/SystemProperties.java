78f145d2ba9914313d9b0fd0073de01b24d4e62c
shuyangzhou <shuyang.zhou@liferay.com>
2015-07-02 09:57:42 -0700

    LPS-56818 Defer loading urls printing after _properties is fully populated, to allow SystemProperties reentrance during classloading (In process executor subprocess context, System.out is swapped to do log piping, and logger initialization needs to access SystemProperties again to read config info, creating a classloading time reentrance to SystemProperties, if the reentrance happens in the middle of the initialization process before the class is not fully initialized, will trigger NPE)

--- portal-service/src/com/liferay/portal/kernel/util/SystemProperties.java
+++ portal-service/src/com/liferay/portal/kernel/util/SystemProperties.java
@@ -19,7 +19,9 @@ import java.io.InputStream;
 
 import java.net.URL;
 
+import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Properties;
@@ -29,6 +31,7 @@ import java.util.concurrent.ConcurrentHashMap;
  * @author Brian Wing Shun Chan
  * @author Mirco Tamburini
  * @author Brett Randall
+ * @author Shuyang Zhou
  */
 public class SystemProperties {
 
@@ -80,8 +83,13 @@ public class SystemProperties {
 
 		ClassLoader classLoader = currentThread.getContextClassLoader();
 
-		boolean systemPropertiesQuiet = GetterUtil.getBoolean(
-			System.getProperty(SYSTEM_PROPERTIES_QUIET));
+		List<URL> urls = null;
+
+		if (!GetterUtil.getBoolean(
+				System.getProperty(SYSTEM_PROPERTIES_QUIET))) {
+
+			urls = new ArrayList<URL>();
+		}
 
 		// system.properties
 
@@ -95,8 +103,8 @@ public class SystemProperties {
 
 				inputStream.close();
 
-				if (!systemPropertiesQuiet) {
-					System.out.println("Loading " + url);
+				if (urls != null) {
+					urls.add(url);
 				}
 			}
 		}
@@ -117,8 +125,8 @@ public class SystemProperties {
 
 				inputStream.close();
 
-				if (!systemPropertiesQuiet) {
-					System.out.println("Loading " + url);
+				if (urls != null) {
+					urls.add(url);
 				}
 			}
 		}
@@ -155,6 +163,12 @@ public class SystemProperties {
 		// java.util.Properties
 
 		PropertiesUtil.fromProperties(properties, _properties);
+
+		if (urls != null) {
+			for (URL url : urls) {
+				System.out.println("Loading " + url);
+			}
+		}
 	}
 
 }
