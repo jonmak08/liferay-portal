ba170db1c637508c6c2b1b769bc2c9ef41c90514
Brian Chan <brian.chan@liferay.com>
2015-07-08 11:10:09 -0700

    LRQA-16080 SF

--- portal-web/test/functional/com/liferay/portalweb/portal/util/liferayselenium/LiferaySeleniumHelper.java
+++ portal-web/test/functional/com/liferay/portalweb/portal/util/liferayselenium/LiferaySeleniumHelper.java
@@ -1276,13 +1276,13 @@ public class LiferaySeleniumHelper {
 			LiferaySelenium liferaySelenium, String image)
 		throws Exception {
 
+		Mouse mouse = new DesktopMouse();
+
 		ScreenRegion screenRegion = new DesktopScreenRegion();
 
 		ImageTarget imageTarget = getImageTarget(liferaySelenium, image);
 
 		screenRegion = screenRegion.find(imageTarget);
-
-		Mouse mouse = new DesktopMouse();
 
 		mouse.click(screenRegion.getCenter());
 	}
