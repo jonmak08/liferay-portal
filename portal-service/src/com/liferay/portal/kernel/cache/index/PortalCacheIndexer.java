9ee84786676ce5c077f5acb81601a4cb7b03322b
Brian Chiu <brian.chiu@liferay.com>
2015-07-15 16:14:45 -0700

    Revert "LPS-54887 Return an empty set instead of null (similar to PortalCache) and make a defensive copy"
    This reverts commit 3ea6de9376ef08f1cf87874d241a9895e60603ef.

--- portal-service/src/com/liferay/portal/kernel/cache/index/PortalCacheIndexer.java
+++ portal-service/src/com/liferay/portal/kernel/cache/index/PortalCacheIndexer.java
@@ -17,7 +17,6 @@ package com.liferay.portal.kernel.cache.index;
 import com.liferay.portal.kernel.cache.CacheListener;
 import com.liferay.portal.kernel.cache.PortalCache;
 
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
@@ -39,13 +38,7 @@ public class PortalCacheIndexer<I, K extends IndexedCacheKey<I>, V> {
 	}
 
 	public Set<K> getIndexedCacheKeys(I index) {
-		Set<K> indexedCacheKeys = _indexedCacheKeys.get(index);
-
-		if (indexedCacheKeys == null) {
-			return Collections.emptySet();
-		}
-
-		return new HashSet<K>(indexedCacheKeys);
+		return _indexedCacheKeys.get(index);
 	}
 
 	public void removeIndexedCacheKeys(I index) {
