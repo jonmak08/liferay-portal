6a3f20ee68a35e20e32459b5158e7ba69f464fe8
Brian Chan <brian.chan@liferay.com>
2015-07-15 14:53:07 -0700

    LRQA-16080 as  used

--- portal-web/test/functional/com/liferay/portalweb/portal/util/liferayselenium/LiferaySeleniumHelper.java
+++ portal-web/test/functional/com/liferay/portalweb/portal/util/liferayselenium/LiferaySeleniumHelper.java
@@ -1278,9 +1278,9 @@ public class LiferaySeleniumHelper {
 
 		Mouse mouse = new DesktopMouse();
 
-		ImageTarget imageTarget = getImageTarget(liferaySelenium, image);
-
 		ScreenRegion screenRegion = new DesktopScreenRegion();
+
+		ImageTarget imageTarget = getImageTarget(liferaySelenium, image);
 
 		ScreenRegion imageTargetScreenRegion = screenRegion.find(imageTarget);
 
@@ -1293,9 +1293,9 @@ public class LiferaySeleniumHelper {
 
 		Mouse mouse = new DesktopMouse();
 
-		ImageTarget imageTarget = getImageTarget(liferaySelenium, image);
-
 		ScreenRegion screenRegion = new DesktopScreenRegion();
+
+		ImageTarget imageTarget = getImageTarget(liferaySelenium, image);
 
 		List<ScreenRegion> imageTargetScreenRegions = screenRegion.findAll(
 			imageTarget);
