61f5633869745bdeea95beb486bd558b47d9f59b
Michael Prigge <michael.prigge@liferay.com>
2015-07-21 14:09:05 -0700

    LPS-50617 Apply discount to items in subcategories

--- portal-impl/src/com/liferay/portlet/shopping/util/ShoppingUtil.java
+++ portal-impl/src/com/liferay/portlet/shopping/util/ShoppingUtil.java
@@ -182,25 +182,26 @@ public class ShoppingUtil {
 
 		String[] categoryNames = StringUtil.split(coupon.getLimitCategories());
 
-		List<Long> categoryIds = new ArrayList<Long>();
+		Set<Long> categoryIds = new HashSet<Long>();
 
 		for (String categoryName : categoryNames) {
 			ShoppingCategory category =
 				ShoppingCategoryLocalServiceUtil.getCategory(
 					coupon.getGroupId(), categoryName);
 
+			List<Long> subcategoryIds = new ArrayList<Long>();
+
+			ShoppingCategoryLocalServiceUtil.getSubcategoryIds(
+				subcategoryIds, category.getGroupId(),
+				category.getCategoryId());
+
 			categoryIds.add(category.getCategoryId());
+			categoryIds.addAll(subcategoryIds);
 		}
 
 		String[] skus = StringUtil.split(coupon.getLimitSkus());
 
 		if ((categoryIds.size() > 0) || (skus.length > 0)) {
-			Set<Long> categoryIdsSet = new HashSet<Long>();
-
-			for (Long categoryId : categoryIds) {
-				categoryIdsSet.add(categoryId);
-			}
-
 			Set<String> skusSet = new HashSet<String>();
 
 			for (String sku : skus) {
@@ -218,8 +219,8 @@ public class ShoppingUtil {
 
 				ShoppingItem item = cartItem.getItem();
 
-				if (((categoryIdsSet.size() > 0) &&
-					 categoryIdsSet.contains(item.getCategoryId())) ||
+				if (((categoryIds.size() > 0) &&
+					 categoryIds.contains(item.getCategoryId())) ||
 					((skusSet.size() > 0) && skusSet.contains(item.getSku()))) {
 
 					newItems.put(cartItem, count);
