b5e0f0d5178dd66f24841402c150e16f929eac08
Zoltán Takács <zoltan.takacs@liferay.com>
2015-08-03 11:07:24 +0200

    LPS-56080 Revert Unrelated SF for fix packs

--- portal-web/docroot/html/js/editor/ckeditor_diffs/plugins/creole/creole_parser.js
+++ portal-web/docroot/html/js/editor/ckeditor_diffs/plugins/creole/creole_parser.js
@@ -136,12 +136,12 @@ Parse.Simple.Base.Rule.prototype = {
                     if (best.index == 0) { break; }
                 }
             }
-
+                
             var pos = best ? best.index : tail.length;
             if (pos > 0) {
                 this.fallback.apply(node, tail.substring(0, pos), options);
             }
-
+            
             if (!best) { break; }
 
             if (!rule.build) { rule = new this.constructor(rule); }
@@ -172,7 +172,7 @@ Parse.Simple.Base.Rule.prototype = {
             }
             node.appendChild(document.createTextNode(data));
         }
-    }
+    }    
 };
 
 Parse.Simple.Base.Rule.prototype.constructor = Parse.Simple.Base.Rule;
@@ -187,7 +187,7 @@ Parse.Simple.Creole = function(options) {
     rx.interwikiPrefix = '[\\w.]+:';
     rx.interwikiLink = rx.interwikiPrefix + rx.link;
     rx.img = '\\{\\{((?!\\{)[^|}\\n]*(?:}(?!})[^|}\\n]*)*)' +
-             (options && options.strict ? '' : '(?:') +
+             (options && options.strict ? '' : '(?:') + 
              '\\|([^}~\\n]*((}(?!})|~.)[^}~\\n]*)*)' +
              (options && options.strict ? '' : ')?') +
              '}}';
@@ -206,7 +206,7 @@ Parse.Simple.Creole = function(options) {
         hr: { tag: 'hr', regex: /(^|\n)\s*----\s*(\n|$)/ },
 
         br: { tag: 'br', regex: /\\\\/ },
-
+        
         preBlock: { tag: 'pre', capture: 2,
             regex: /(^|\n)\{\{\{\n((.*\n)*?)\}\}\}(\n|$)/,
             replaceRegex: /^ ([ \t]*\}\}\})/gm,
@@ -281,14 +281,14 @@ Parse.Simple.Creole = function(options) {
         namedLink: { regex: '\\[\\[(' + rx.link + ')\\|(' + rx.linkText + ')\\]\\]',
             build: function(node, r, options) {
                 var link = document.createElement('a');
-
+                
                 link.href = options && options.linkFormat
                     ? formatLink(r[1].replace(/~(.)/g, '$1'), options.linkFormat)
                     : r[1].replace(/~(.)/g, '$1');
 link.setAttribute('data-cke-saved-href', link.href);
 
                 this.apply(link, r[2], options);
-
+                
                 node.appendChild(link);
             } },
 
@@ -318,13 +318,13 @@ link.setAttribute('data-cke-saved-href', link.href);
     g.namedInterwikiLink = { regex: '\\[\\[(' + rx.interwikiLink + ')\\|(' + rx.linkText + ')\\]\\]',
         build: function(node, r, options) {
                 var link = document.createElement('a');
-
+                
                 var m, f;
                 if (options && options.interwiki) {
                 m = r[1].match(/(.*?):(.*)/);
                 f = options.interwiki[m[1]];
             }
-
+            
             if (typeof f == 'undefined') {
                 if (!g.namedLink.apply) {
                     g.namedLink = new this.constructor(g.namedLink);
@@ -333,9 +333,9 @@ link.setAttribute('data-cke-saved-href', link.href);
             }
 
             link.href = formatLink(m[2].replace(/~(.)/g, '$1'), f);
-
+            
             this.apply(link, r[2], options);
-
+            
             node.appendChild(link);
         }
     };
