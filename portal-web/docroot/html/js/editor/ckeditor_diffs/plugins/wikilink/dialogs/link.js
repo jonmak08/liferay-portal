493d4896334270bc2098b66d13cff50089204f58
Turcsán Csaba <csaba.turcsan@liferay.com>
2015-08-06 13:51:39 +0200

    LPS-56644  :
    - consider blank start or end and consider domain and IP. Extract logic to function to reuse
    - fill protocal in UI to make it clear to user
    - fix for chrome. Chrome adds "\u200B" to string when use shortcut to paste
    - use indexOf method since Ie Opera and Safari does not support startsWith method see (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/startsWith#Browser_compatibility)
    - Update if statements.

--- portal-web/docroot/html/js/editor/ckeditor_diffs/plugins/wikilink/dialogs/link.js
+++ portal-web/docroot/html/js/editor/ckeditor_diffs/plugins/wikilink/dialogs/link.js
@@ -7,6 +7,20 @@ CKEDITOR.dialog.add(
 
 		var PLUGIN = CKEDITOR.plugins.link;
 
+		var handleAddress = function(val) {
+			var address = val.toString().trim();
+
+			address = address.replace(/[\u200B-\u200D\uFEFF]/g, '');
+
+			var prefix = '';
+
+			if (address.indexOf('/') !== 0 && address.indexOf('://') === -1) {
+				prefix = 'http://';
+			}
+
+			return prefix + address;
+		};
+
 		var parseLink = function(editor, element) {
 			var instance = this;
 
@@ -29,6 +43,8 @@ CKEDITOR.dialog.add(
 
 				data.address = selection.getSelectedText();
 			}
+
+			data.address = handleAddress(data.address);
 
 			instance._.selectedElement = element;
 
@@ -53,9 +69,7 @@ CKEDITOR.dialog.add(
 
 										var address = val;
 
-										if (val.indexOf('www.') === 0) {
-											address = 'http://' + val;
-										}
+										address = handleAddress(address);
 
 										data.address = address;
 										data.text = val;
