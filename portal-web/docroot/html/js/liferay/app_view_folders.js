b5c724381f291620cb9a6ae81af96915fa723a4f
austinchiang <austin.chiang@liferay.com>
2015-08-06 14:36:04 -0700

    Revert "LPS-55306 SF"
    This reverts commit 36b29fd59c3181f7f1f16445d7e305bd5ea330c9.

--- portal-web/docroot/html/js/liferay/app_view_folders.js
+++ portal-web/docroot/html/js/liferay/app_view_folders.js
@@ -571,19 +571,15 @@ AUI.add(
 						}
 
 						var sortButtonContainer = instance.byId('sortButtonContainer');
-
 						var manageButtonContainer = instance.byId('manageButtonContainer');
-
 						var newSortButtonContainer = instance.one('#sortButton #' + instance.NS + 'sortButtonContainer', content);
 
 						if (newSortButtonContainer) {
 							manageButtonContainer.show();
-
 							sortButtonContainer.replace(newSortButtonContainer);
 						}
 						else {
 							manageButtonContainer.hide();
-
 							sortButtonContainer.empty();
 						}
 					},
