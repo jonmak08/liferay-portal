aaa1034e0242651ace157778e117b78ca1ff728b
Turcsán Csaba <csaba.turcsan@liferay.com>
2015-08-27 15:39:11 +0200

    Revert "LPS-53961 SF"
    This reverts commit 956b5b528fdbe06db302fe60b6d61c01bf3f2903.

--- portal-impl/test/integration/com/liferay/portlet/bookmarks/permission/BookmarksEntryPermissionTest.java
+++ portal-impl/test/integration/com/liferay/portlet/bookmarks/permission/BookmarksEntryPermissionTest.java
@@ -96,7 +96,7 @@ public class BookmarksEntryPermissionTest {
 	}
 
 	@Test
-	public void testGetRoles() throws Exception {
+	public void testGetRolesWithPermission() throws Exception {
 		TestDocument testDocument = new TestDocument(
 			_bookmarksEntry1.getGroupId(), _bookmarksEntry1.getEntryId());
 
