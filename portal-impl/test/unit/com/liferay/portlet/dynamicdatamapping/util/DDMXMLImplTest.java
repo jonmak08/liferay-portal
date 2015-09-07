664b88b700a98480fb34ece18439aff362f339c2
Hudak Laszlo <laszlo.hudak@liferay.com>
2015-09-07 10:28:07 +0200

    LPS-45702 Refactoring  of previous tests for avoiding code duplication

--- portal-impl/test/unit/com/liferay/portlet/dynamicdatamapping/util/DDMXMLImplTest.java
+++ portal-impl/test/unit/com/liferay/portlet/dynamicdatamapping/util/DDMXMLImplTest.java
@@ -15,16 +15,13 @@
 package com.liferay.portlet.dynamicdatamapping.util;
 
 import com.liferay.portal.kernel.util.LocaleUtil;
-import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.xml.Attribute;
 import com.liferay.portal.kernel.xml.Document;
 import com.liferay.portal.kernel.xml.Element;
 import com.liferay.portal.kernel.xml.Node;
-import com.liferay.portal.kernel.xml.SAXReaderUtil;
 import com.liferay.portal.kernel.xml.UnsecureSAXReaderUtil;
-import com.liferay.portal.util.PropsImpl;
-import com.liferay.portal.xml.SAXReaderImpl;
+import com.liferay.portlet.dynamicdatamapping.BaseDDMTest;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -34,50 +31,13 @@ import java.util.Locale;
 
 import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
 /**
  * @author Manuel de la Peña
+ * @author Miguel Angelo Caldas Gallindo
  */
-@PrepareForTest({DDMXMLUtil.class})
-@RunWith(PowerMockRunner.class)
-public class DDMXMLImplTest extends PowerMockito {
-
-	@Before
-	public void setUp() {
-		PropsUtil.setProps(new PropsImpl());
-
-		SAXReaderUtil saxReaderUtil = new SAXReaderUtil();
-
-		SAXReaderImpl secureSAXReader = new SAXReaderImpl();
-
-		secureSAXReader.setSecure(true);
-
-		saxReaderUtil.setSAXReader(secureSAXReader);
-
-		saxReaderUtil.setSAXReader(secureSAXReader);
-
-		UnsecureSAXReaderUtil unsecureSAXReaderUtil =
-			new UnsecureSAXReaderUtil();
-
-		SAXReaderImpl unsecureSAXReader = new SAXReaderImpl();
-
-		unsecureSAXReaderUtil.setSAXReader(unsecureSAXReader);
-
-		spy(DDMXMLUtil.class);
-
-		when(
-			DDMXMLUtil.getDDMXML()
-		).thenReturn(
-			_ddmXML
-		);
-	}
+public class DDMXMLImplTest extends BaseDDMTest {
 
 	@After
 	public void tearDown() {
@@ -187,7 +147,5 @@ public class DDMXMLImplTest extends PowerMockito {
 
 		Assert.assertFalse(expectedResult);
 	}
-
-	private DDMXMLImpl _ddmXML = new DDMXMLImpl();
 
 }
