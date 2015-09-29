0f525e002d061dd68e6498113fdb0d3965f824a4
Eric Yan <eric.yan@liferay.com>
2015-09-29 09:34:04 -0700

    LPS-49766 Move the property to Web Server Servlet section and retain the property value as true for backport to 62x. This will preserve the check for instances prior to portal 6.1 and allow newer instances to set the property separately in their portal-ext.properties.

--- portal-service/src/com/liferay/portal/kernel/util/PropsKeys.java
+++ portal-service/src/com/liferay/portal/kernel/util/PropsKeys.java
@@ -667,8 +667,6 @@ public interface PropsKeys {
 
 	public static final String DL_EMAIL_FROM_NAME = "dl.email.from.name";
 
-	public static final String DL_FILE_ENTRY_CHECK_LEGACY_IG_REQUESTS_ENABLED = "dl.file.entry.check.legacy.ig.requests.enabled";
-
 	public static final String DL_FILE_ENTRY_COMMENTS_ENABLED = "dl.file.entry.comments.enabled";
 
 	public static final String DL_FILE_ENTRY_CONVERSIONS_ENABLED = "dl.file.entry.conversions.enabled";
@@ -2714,6 +2712,8 @@ public interface PropsKeys {
 	public static final String WEB_SERVER_PROXY_LEGACY_MODE = "web.server.proxy.legacy.mode";
 
 	public static final String WEB_SERVER_SERVLET_ACCEPT_RANGES_MIME_TYPES = "web.server.servlet.accept.ranges.mime.types";
+
+	public static final String WEB_SERVER_SERVLET_CHECK_LEGACY_IMAGE_GALLERY_REQUEST_ENABLED = "web.server.servlet.check.legacy.image.gallery.request.enabled";
 
 	public static final String WEB_SERVER_SERVLET_DIRECTORY_INDEXING_ENABLED = "web.server.servlet.directory.indexing.enabled";
 
