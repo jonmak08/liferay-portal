04d37192dc3cc67112851da40f0e5b4f1a9055ca
dustinryerson <dustin.ryerson@liferay.com>
2015-10-05 12:41:05 -0700

    LPS-51378 SF

--- portal-impl/src/com/liferay/portal/search/SearchPermissionCheckerImpl.java
+++ portal-impl/src/com/liferay/portal/search/SearchPermissionCheckerImpl.java
@@ -435,10 +435,10 @@ public class SearchPermissionCheckerImpl implements SearchPermissionChecker {
 				for (long groupId : groupIds) {
 					if (ResourcePermissionLocalServiceUtil.
 							hasResourcePermission(
-									companyId, className,
-									ResourceConstants.SCOPE_GROUP,
-									String.valueOf(groupId), role.getRoleId(),
-									ActionKeys.VIEW)) {
+								companyId, className,
+								ResourceConstants.SCOPE_GROUP,
+								String.valueOf(groupId), role.getRoleId(),
+								ActionKeys.VIEW)) {
 
 						groupsQuery.addTerm(Field.GROUP_ID, groupId);
 					}
