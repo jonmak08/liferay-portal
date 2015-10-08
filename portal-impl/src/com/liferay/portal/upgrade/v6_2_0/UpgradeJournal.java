c7776ba305e2bc46787cabfb6ce5113df5b887dc
Alberto Chaparro <alberto.chaparro@liferay.com>
2015-10-08 11:11:37 +0200

    LPS-59254 Set company default locale for 6.0.x journal structures too

--- portal-impl/src/com/liferay/portal/upgrade/v6_2_0/UpgradeJournal.java
+++ portal-impl/src/com/liferay/portal/upgrade/v6_2_0/UpgradeJournal.java
@@ -20,7 +20,6 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.upgrade.BaseUpgradePortletPreferences;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
-import com.liferay.portal.kernel.util.LocalizationUtil;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
@@ -102,7 +101,8 @@ public class UpgradeJournal extends BaseUpgradePortletPreferences {
 			ps.setString(13, description);
 			ps.setString(
 				14,
-				JournalConverterImpl.getDDMXSD(xsd, getDefaultLocale(name)));
+				JournalConverterImpl.getDDMXSD(
+					xsd, getCompanyDefaultLocale(companyId)));
 			ps.setString(15, storageType);
 			ps.setInt(16, type);
 
@@ -310,10 +310,33 @@ public class UpgradeJournal extends BaseUpgradePortletPreferences {
 		return getDDMStructureId(groupId, 0, structureId, warn);
 	}
 
-	protected Locale getDefaultLocale(String xml) {
-		String defaultLanguageId = LocalizationUtil.getDefaultLanguageId(xml);
+	protected Locale getCompanyDefaultLocale(long companyId) throws Exception {
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
 
-		return LocaleUtil.fromLanguageId(defaultLanguageId);
+		try {
+			con = DataAccess.getUpgradeOptimizedConnection();
+
+			ps = con.prepareStatement(
+				"select languageId FROM User_ WHERE defaultUser = 1 and " +
+					"companyId = ?");
+
+			ps.setLong(1, companyId);
+
+			rs = ps.executeQuery();
+
+			if (rs.next()) {
+				String languageId = rs.getString("languageId");
+
+				return LocaleUtil.fromLanguageId(languageId);
+			}
+		}
+		finally {
+			DataAccess.cleanUp(con, ps, rs);
+		}
+
+		return LocaleUtil.getSiteDefault();
 	}
 
 	@Override
