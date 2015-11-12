7a24c4f938bad37565bf1f2339079ffa0ce058c8
shuyangzhou <shuyang.zhou@liferay.com>
2015-11-12 13:58:14 -0800

    LPS-60473 Simplify the checking, count will always be 0 if the current db does not support batch update.

--- portal-impl/src/com/liferay/portal/upgrade/util/Table.java
+++ portal-impl/src/com/liferay/portal/upgrade/util/Table.java
@@ -516,12 +516,10 @@ public class Table {
 				}
 			}
 
-			if (databaseMetaData.supportsBatchUpdates()) {
-				if (count != 0) {
-					ps.executeBatch();
+			if (count != 0) {
+				ps.executeBatch();
 
-					ps.close();
-				}
+				ps.close();
 			}
 		}
 		finally {
