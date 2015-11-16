4d1ceffd4187d42d62a525e3ffb5c09def63a770
samanzanedo <sergio.alonso@liferay.com>
2015-11-16 15:46:47 +0100

    LPS-54188 Changed util method to form custom validator name. Remove not used method.

--- util-taglib/src/com/liferay/taglib/aui/ValidatorTagImpl.java
+++ util-taglib/src/com/liferay/taglib/aui/ValidatorTagImpl.java
@@ -15,12 +15,14 @@
 package com.liferay.taglib.aui;
 
 import com.liferay.portal.kernel.servlet.taglib.aui.ValidatorTag;
+import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
-import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.model.ModelHintsUtil;
+import com.liferay.portal.util.PortalUtil;
 import com.liferay.taglib.aui.base.BaseValidatorTagImpl;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.tagext.BodyContent;
 import javax.servlet.jsp.tagext.BodyTag;
 
@@ -73,7 +75,20 @@ public class ValidatorTagImpl
 		_custom = ModelHintsUtil.isCustomValidator(name);
 
 		if (_custom) {
-			name = ModelHintsUtil.buildCustomValidatorName(name);
+			StringBundler sb = new StringBundler(3);
+
+			String namespace = inputTag.getName();
+
+			sb.append(namespace);
+
+			sb.append(StringPool.UNDERLINE);
+
+			HttpServletRequest request =
+				(HttpServletRequest)pageContext.getRequest();
+
+			sb.append(PortalUtil.getUniqueElementId(request, namespace, name));
+
+			name = sb.toString();
 		}
 
 		ValidatorTag validatorTag = new ValidatorTagImpl(
@@ -113,18 +128,6 @@ public class ValidatorTagImpl
 		_body = body;
 	}
 
-	protected String processCustom(String name) {
-		if (name.equals("custom")) {
-			_custom = true;
-
-			return name.concat(StringPool.UNDERLINE).concat(
-				StringUtil.randomId());
-		}
-
-		return name;
-	}
-
 	private String _body;
 	private boolean _custom;
-
 }
