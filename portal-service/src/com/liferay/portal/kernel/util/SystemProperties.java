10df012c6999b615e2654ac92a6a3e501f728f0f
Tibor Lipusz <tibor.lipusz@liferay.com>
2015-12-01 10:55:43 +0100

    LPS-60637 Update deprecation

--- portal-service/src/com/liferay/portal/kernel/util/SystemProperties.java
+++ portal-service/src/com/liferay/portal/kernel/util/SystemProperties.java
@@ -89,7 +89,7 @@ public class SystemProperties {
 	}
 
 	/**
-	 * @deprecated As of 7.0.0, with no direct replacement
+	 * @deprecated As of 7.0.0, replaced by {@link #load(ClassLoader)}
 	 */
 	@Deprecated
 	public static void reload() {
