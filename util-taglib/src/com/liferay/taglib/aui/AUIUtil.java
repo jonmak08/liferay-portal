67a735161afee4daa92b9584fd90237173e3453e
Eric Yan <eric.yan@liferay.com>
2016-01-13 10:16:28 -0800

    LPS-50059 - Source formatting

--- util-taglib/src/com/liferay/taglib/aui/AUIUtil.java
+++ util-taglib/src/com/liferay/taglib/aui/AUIUtil.java
@@ -156,14 +156,12 @@ public class AUIUtil {
 		}
 
 		if (baseType.equals("checkbox") || baseType.equals("radio")) {
-			sb.append("class=\"");
-			sb.append(baseType);
-
 			if (inlineField) {
-				sb.append(" inline");
+				sb.append("class=\"");
+				sb.append(baseType);
+				sb.append("-inline");
+				sb.append("\" ");
 			}
-
-			sb.append("\" ");
 		}
 		else {
 			sb.append("class=\"control-label\" ");
