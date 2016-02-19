1a396cd2e7e2b95beb0898d94fefa26b5a4fda88
Zsolt Szabo <zsolt.szabo@liferay.com>
2016-02-19 10:28:03 +0100

    LPS-63444 Breadcrumb link URL generation: no space before attributes starting with "data-"

--- util-taglib/src/com/liferay/taglib/ui/BreadcrumbTag.java
+++ util-taglib/src/com/liferay/taglib/ui/BreadcrumbTag.java
@@ -297,7 +297,7 @@ public class BreadcrumbTag extends IncludeTag {
 				if (Validator.isNotNull(breadcrumbURL)) {
 					sb.append("<a href=\"");
 					sb.append(HtmlUtil.escape(breadcrumbURL));
-					sb.append("\"");
+					sb.append("\" ");
 					sb.append(AUIUtil.buildData(data));
 					sb.append(">");
 				}
