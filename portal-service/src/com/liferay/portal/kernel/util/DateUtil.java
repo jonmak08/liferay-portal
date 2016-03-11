3a44d764b3d308b699b5dc670984c4e74b26b97c
Michael Bowerman <michael.bowerman@liferay.com>
2016-03-11 10:58:54 -0800

    LPS-64179 Create utilities methods to parse/format Dates

--- portal-service/src/com/liferay/portal/kernel/util/DateUtil.java
+++ portal-service/src/com/liferay/portal/kernel/util/DateUtil.java
@@ -88,6 +88,17 @@ public class DateUtil {
 		return false;
 	}
 
+	public static String formatDate(
+			String fromPattern, String dateString, Locale locale)
+		throws ParseException {
+
+		Date dateValue = parseDate(fromPattern, dateString, locale);
+
+		Format dateFormat = FastDateFormatFactoryUtil.getDate(locale);
+
+		return dateFormat.format(dateValue);
+	}
+
 	public static String getCurrentDate(String pattern, Locale locale) {
 		return getDate(new Date(), pattern, locale);
 	}
@@ -264,8 +275,22 @@ public class DateUtil {
 	public static Date parseDate(String dateString, Locale locale)
 		throws ParseException {
 
-		DateFormat dateFormat = DateFormat.getDateInstance(
-			DateFormat.SHORT, locale);
+		return parseDate(null, dateString, locale);
+	}
+
+	public static Date parseDate(
+			String pattern, String dateString, Locale locale)
+		throws ParseException {
+
+		DateFormat dateFormat = null;
+
+		if (Validator.isNull(pattern)) {
+			dateFormat = DateFormat.getDateInstance(DateFormat.SHORT, locale);
+		}
+		else {
+			dateFormat = DateFormatFactoryUtil.getSimpleDateFormat(
+				pattern, locale);
+		}
 
 		return dateFormat.parse(dateString);
 	}
