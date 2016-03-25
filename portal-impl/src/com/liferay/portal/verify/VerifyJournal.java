b2831b3607de684f7a65e4397fa8aaaad0c94ddc
Dustin Ryerson <dustin.ryerson@liferay.com>
2016-03-25 11:14:56 -0700

    LPS-64179 SF

--- portal-impl/src/com/liferay/portal/verify/VerifyJournal.java
+++ portal-impl/src/com/liferay/portal/verify/VerifyJournal.java
@@ -599,13 +599,14 @@ public class VerifyJournal extends VerifyProcess {
 			if (Validator.isNull(dynamicElement.attribute("type"))) {
 				if (structure == null) {
 					long groupId = article.getGroupId();
+
 					long classNameId = PortalUtil.getClassNameId(
 						JournalArticle.class);
+
 					String structureKey = article.getStructureId();
 
-					structure =
-						DDMStructureLocalServiceUtil.getStructure(
-							groupId, classNameId, structureKey);
+					structure = DDMStructureLocalServiceUtil.getStructure(
+						groupId, classNameId, structureKey);
 				}
 
 				updateElementTypeParameter(structure, dynamicElement);
