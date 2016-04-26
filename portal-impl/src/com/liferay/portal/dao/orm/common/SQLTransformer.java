9c389d8ce1bcc3a9a5d94e028b0abf2dd77d4c1b
dsanz <daniel.sanz@liferay.com>
2016-04-26 09:55:54 +0200

    LPS-65538 Add CAST_CLOB_TEXT pseudo function to SQLTransformer. Partial backport LPS-56674 (commit 6842892c67edb091a85156019b7ea5be828aaa52)

--- portal-impl/src/com/liferay/portal/dao/orm/common/SQLTransformer.java
+++ portal-impl/src/com/liferay/portal/dao/orm/common/SQLTransformer.java
@@ -188,6 +188,16 @@ public class SQLTransformer {
 			new String[] {_db.getTemplateFalse(), _db.getTemplateTrue()});
 	}
 
+	private String _replaceCastClobText(String sql) {
+		Matcher matcher = _castClobTextPattern.matcher(sql);
+
+		if (_vendorOracle) {
+			return matcher.replaceAll("DBMS_LOB.SUBSTR($1, 1, 4000)");
+		}
+
+		return _replaceCastText(matcher);
+	}
+
 	private String _replaceCastLong(String sql) {
 		Matcher matcher = _castLongPattern.matcher(sql);
 
@@ -203,8 +213,10 @@ public class SQLTransformer {
 	}
 
 	private String _replaceCastText(String sql) {
-		Matcher matcher = _castTextPattern.matcher(sql);
+		return _replaceCastText(_castTextPattern.matcher(sql));
+	}
 
+	private String _replaceCastText(Matcher matcher) {
 		if (_vendorDB2 || _vendorDerby) {
 			return matcher.replaceAll("CAST($1 AS CHAR(254))");
 		}
@@ -299,6 +311,7 @@ public class SQLTransformer {
 
 		newSQL = _replaceBitwiseCheck(newSQL);
 		newSQL = _replaceBoolean(newSQL);
+		newSQL = _replaceCastClobText(newSQL);
 		newSQL = _replaceCastLong(newSQL);
 		newSQL = _replaceCastText(newSQL);
 		newSQL = _replaceCrossJoin(newSQL);
@@ -429,6 +442,8 @@ public class SQLTransformer {
 
 	private static Pattern _bitwiseCheckPattern = Pattern.compile(
 		"BITAND\\((.+?),(.+?)\\)");
+	private static final Pattern _castClobTextPattern = Pattern.compile(
+		"CAST_CLOB_TEXT\\((.+?)\\)", Pattern.CASE_INSENSITIVE);
 	private static Pattern _castLongPattern = Pattern.compile(
 		"CAST_LONG\\((.+?)\\)", Pattern.CASE_INSENSITIVE);
 	private static Pattern _castTextPattern = Pattern.compile(
