df057be34f595ef09c7cc8e5b0eb7a9c565745bc
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2016-06-06 19:48:51 -0300

    LPS-63813 Check for permission when assigning task.

--- portal-impl/src/com/liferay/portlet/workflowtasks/action/EditWorkflowTaskAction.java
+++ portal-impl/src/com/liferay/portlet/workflowtasks/action/EditWorkflowTaskAction.java
@@ -22,6 +22,7 @@ import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.kernel.workflow.WorkflowException;
+import com.liferay.portal.kernel.workflow.WorkflowTask;
 import com.liferay.portal.kernel.workflow.WorkflowTaskDueDateException;
 import com.liferay.portal.kernel.workflow.WorkflowTaskManagerUtil;
 import com.liferay.portal.security.auth.PrincipalException;
@@ -142,6 +143,19 @@ public class EditWorkflowTaskAction extends PortletAction {
 		long assigneeUserId = ParamUtil.getLong(
 			actionRequest, "assigneeUserId");
 		String comment = ParamUtil.getString(actionRequest, "comment");
+
+		WorkflowTask workflowTask = WorkflowTaskManagerUtil.getWorkflowTask(
+			themeDisplay.getCompanyId(), workflowTaskId);
+
+		if (!WorkflowTaskPermissionChecker.hasAssignmentPermission(
+				themeDisplay.getScopeGroupId(), workflowTask,
+				themeDisplay.getPermissionChecker())) {
+
+			throw new PrincipalException(
+				"User " + themeDisplay.getUserId() + " does not have " +
+				"permissions to assign the task " + workflowTaskId +
+				"to someone.");
+		}
 
 		WorkflowTaskManagerUtil.assignWorkflowTaskToUser(
 			themeDisplay.getCompanyId(), themeDisplay.getUserId(),
