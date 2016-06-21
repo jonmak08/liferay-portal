0f6063debcaca551c3305c9163f4f6bff591322a
Michael Prigge <michael.prigge@liferay.com>
2016-06-21 14:44:27 -0700

    LPS-33201 We need to call AutoReset

--- util-taglib/src/com/liferay/taglib/portletext/RuntimeTag.java
+++ util-taglib/src/com/liferay/taglib/portletext/RuntimeTag.java
@@ -26,6 +26,7 @@ import com.liferay.portal.kernel.portlet.PortletParameterUtil;
 import com.liferay.portal.kernel.portlet.RestrictPortletServletRequest;
 import com.liferay.portal.kernel.servlet.DynamicServletRequest;
 import com.liferay.portal.kernel.servlet.PipingServletResponse;
+import com.liferay.portal.kernel.util.AutoResetThreadLocal;
 import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.PrefixPredicateFilter;
 import com.liferay.portal.kernel.util.StringPool;
@@ -257,7 +258,8 @@ public class RuntimeTag extends TagSupport {
 	private static Log _log = LogFactoryUtil.getLog(RuntimeTag.class);
 
 	private static final ThreadLocal<Stack<String>> _embeddedPortletIds =
-		new ThreadLocal<>();
+		new AutoResetThreadLocal<>(
+			RuntimeTag.class + "._embeddedPortletIds");
 
 	private String _defaultPreferences;
 	private String _portletName;
