251b22eb9f7edc273b59a7b8cc3b9f4c34dfe9a6
Bryan Engler <bryan.engler@liferay.com>
2016-08-02 15:02:51 -0700

    LPS-67435 Partial backport of LPS-40841 (e86697a). Removes associated images when web content article is deleted

--- portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -957,8 +957,14 @@ public class JournalArticleLocalServiceImpl
 
 		// Images
 
+		String articleId = article.getArticleId();
+
+		if (article.isInTrash()) {
+			articleId = TrashUtil.getOriginalTitle(articleId);
+		}
+
 		journalArticleImageLocalService.deleteImages(
-			article.getGroupId(), article.getArticleId(), article.getVersion());
+			article.getGroupId(), articleId, article.getVersion());
 
 		// Expando
 
