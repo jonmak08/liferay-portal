714e4b0bc59d5c3c6e6ec0afd691b644f4302cfd
ChrisKian <Christopher.Kian@liferay.com>
2016-10-04 16:58:26 -0700

    LPS-57942 SF

--- portal-impl/src/com/liferay/portal/security/auth/LDAPAuth.java
+++ portal-impl/src/com/liferay/portal/security/auth/LDAPAuth.java
@@ -286,10 +286,12 @@ public class LDAPAuth implements Authenticator {
 				User user = PortalLDAPImporterUtil.importLDAPUser(
 						ldapServerId, companyId, ldapContext, attributes, password);
 
-				// Process LDAP failure codes
+				// Authenticate
 
 				LDAPAuthResult ldapAuthResult = authenticate(
 					ldapContext, companyId, attributes, fullUserDN, password);
+
+				// Process LDAP failure codes
 
 				String errorMessage = ldapAuthResult.getErrorMessage();
 
