b35c4cf25740b556dd1da689e88fb273dd404626
Michael Bowerman <michael.bowerman@liferay.com>
2016-10-06 15:02:55 -0700

    LPS-68572 Make a copy of the map first so the original map remains unmodified and can be reused

--- portal-impl/src/com/liferay/portal/service/impl/ResourcePermissionLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/ResourcePermissionLocalServiceImpl.java
@@ -1263,21 +1263,24 @@ public class ResourcePermissionLocalServiceImpl
 					companyId, name, scope, primKey,
 					ArrayUtil.subset(roleIds, start, roleIds.length)));
 
+			Map<Long, String[]> roleIdsToActionIdsCopy = new HashMap<>(
+				roleIdsToActionIds);
+
 			for (ResourcePermission resourcePermission : resourcePermissions) {
 				long roleId = resourcePermission.getRoleId();
-				String[] actionIds = roleIdsToActionIds.remove(roleId);
+				String[] actionIds = roleIdsToActionIdsCopy.remove(roleId);
 
 				doUpdateResourcePermission(
 					companyId, name, scope, primKey, ownerId, roleId, actionIds,
 					ResourcePermissionConstants.OPERATOR_SET, true);
 			}
 
-			if (roleIdsToActionIds.isEmpty()) {
+			if (roleIdsToActionIdsCopy.isEmpty()) {
 				return;
 			}
 
 			for (Map.Entry<Long, String[]> entry :
-					roleIdsToActionIds.entrySet()) {
+					roleIdsToActionIdsCopy.entrySet()) {
 
 				long roleId = entry.getKey();
 				String[] actionIds = entry.getValue();
