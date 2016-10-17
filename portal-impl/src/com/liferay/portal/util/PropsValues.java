796fd53345b5af51cad7053559d55352e0caa787
Tibor Lipusz <tibor.lipusz@liferay.com>
2016-10-17 15:06:12 +0200

    LPS-67124 Make new property hookable so pluggable search adapters like Solr can also disable it

--- portal-impl/src/com/liferay/portal/util/PropsValues.java
+++ portal-impl/src/com/liferay/portal/util/PropsValues.java
@@ -858,7 +858,7 @@ public class PropsValues {
 
 	public static final boolean INDEX_ON_UPGRADE = GetterUtil.getBoolean(PropsUtil.get(PropsKeys.INDEX_ON_UPGRADE));
 
-	public static final boolean INDEX_PORTAL_FIELD_ANALYZER_ENABLED = GetterUtil.getBoolean(PropsUtil.get(PropsKeys.INDEX_PORTAL_FIELD_ANALYZER_ENABLED));
+	public static boolean INDEX_PORTAL_FIELD_ANALYZER_ENABLED = GetterUtil.getBoolean(PropsUtil.get(PropsKeys.INDEX_PORTAL_FIELD_ANALYZER_ENABLED));
 
 	public static boolean INDEX_READ_ONLY = GetterUtil.getBoolean(PropsUtil.get(PropsKeys.INDEX_READ_ONLY));
 
