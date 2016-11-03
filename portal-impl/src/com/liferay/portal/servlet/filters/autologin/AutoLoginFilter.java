a7b74e8ee1b268a251556070b83bce22b6e91641
Tibor Lipusz <tibor.lipusz@liferay.com>
2016-11-03 16:39:53 +0100

    LPS-68662 Parameters in an URL should be URL encoded

--- portal-impl/src/com/liferay/portal/servlet/filters/autologin/AutoLoginFilter.java
+++ portal-impl/src/com/liferay/portal/servlet/filters/autologin/AutoLoginFilter.java
@@ -18,6 +18,7 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.servlet.ProtectedServletRequest;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HttpUtil;
 import com.liferay.portal.kernel.util.InstancePool;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
@@ -137,8 +138,10 @@ public class AutoLoginFilter extends BasePortalFilter {
 					redirect = redirect.concat(autoLoginRedirect);
 				}
 				else {
-					redirect = redirect.concat(
+					String currentURL = HttpUtil.encodeURL(
 						PortalUtil.getCurrentCompleteURL(request));
+
+					redirect = redirect.concat(currentURL);
 				}
 			}
 
