2ba84426cc0eeb8f514b305125044d68cd39076e
Joshua Wu <joshua.wu@liferay.com>
2017-05-24 16:24:55 -0700

    updated js file to optimize code

--- modules/apps/josh-lapurd-theme/src/js/main.js
+++ modules/apps/josh-lapurd-theme/src/js/main.js
@@ -1,69 +1,65 @@
 AUI().ready(
-    'liferay-sign-in-modal',
-    function( A ) {
-        var signIn = A.one( '#signIn' );
+	'liferay-sign-in-modal',
+	'scroll-transparency',
+	function(A) {
+		A.on(
+			'scroll',
+			function() {
+				var banner = AUI.$('#banner');
+				var buttonBtt = AUI.$('.btn-back-to-top');
+				var scrollY = window.pageYOffset;
 
-        if ( signIn && signIn.getData( 'redirect' ) !== 'true' ) {
-            signIn.plug( Liferay.SignInModal );
-        }
-    }
+				if (scrollY > 350 && banner.hasClass('banner-unscrolled')) {
+					banner.toggleClass('banner-scrolled banner-unscrolled');
+					buttonBtt.fadeIn();
+				}
+					else if (scrollY <= 350 && banner.hasClass('banner-scrolled')) {
+						banner.toggleClass('banner-scrolled banner-unscrolled');
+						buttonBtt.fadeOut();
+					}
+			}
+		);
+
+		var signIn = A.one('#signIn');
+
+		if (signIn && signIn.getData('redirect') !== 'true') {
+			signIn.plug(Liferay.SignInModal);
+		}
+	}
 );
 
-AUI.$( 'a.theme-nav-bar-toggle' ).click(
-    function() {
-        AUI.$( '#themeNavBar' ).toggleClass( 'menu-open' );
-    }
+function toggleOpenClose() {
+	var currTarget = AUI.$(this);
+	var currTarSib = currTarget.siblings('ul.child-menu');
+
+	if (currTarSib !== undefined) {
+		currTarSib.toggleClass('child-menu-open');
+		currTarget.toggleClass('child-menu-toggle-open');
+	}
+}
+
+AUI.$('a.theme-nav-bar-toggle').click(
+	function() {
+		AUI.$('#themeNavBar').toggleClass('menu-open');
+	}
 );
 
-AUI.$( '#childMenuDropdown_1' ).click(
-    function() {
-        var currTarget = AUI.$( this );
-        var currTarSib = currTarget.siblings( 'ul.child-menu' );
-
-        if ( currTarSib !== undefined ) {
-            currTarSib.toggleClass( 'child-menu-open' );
-            currTarget.toggleClass( 'child-menu-toggle-open' );
-        }
-    }
+AUI.$('#childMenuDropdown_1').click(
+	toggleOpenClose
 );
 
-AUI.$( '#childMenuDropdown_2' ).click(
-    function() {
-        var currTarget = AUI.$( this );
-        var currTarSib = currTarget.siblings( 'ul.child-menu' );
-
-        if ( currTarSib !== undefined ) {
-            currTarSib.toggleClass( 'child-menu-open' );
-            currTarget.toggleClass( 'child-menu-toggle-open' );
-        }
-    }
+AUI.$('#childMenuDropdown_2').click(
+	toggleOpenClose
 );
 
-AUI.$( '.btn-btt' ).click(
-    function() {
-        AUI.$( 'html, body' ).animate( 
-            { scrollTop : 0 }, 
-            700
-        );
-        return false;
-    }
-);
-
-AUI().ready(
-    'scroll-transparency',
-    function( A ) {
-        A.on( 'scroll', function() {
-            var scrollY = window.pageYOffset;
-            var buttonBtt = AUI.$( '.btn-btt' );
-            var banner = AUI.$( '#banner' );
-
-            if ( scrollY > 0 && banner.hasClass( 'banner-unscrolled' ) ) {
-                banner.toggleClass( 'banner-scrolled banner-unscrolled' );
-                buttonBtt.fadeIn();
-            } else if ( scrollY === 0 && banner.hasClass( 'banner-scrolled' ) ) {
-                banner.toggleClass( 'banner-scrolled banner-unscrolled' );
-                buttonBtt.fadeOut();
-            }
-        } );
-    }
+AUI.$('.btn-back-to-top').click(
+	function() {
+		AUI.$('html, body').animate(
+			{
+				scrollTop: 0
+			},
+			700
+		);
+		return false;
+	}
 );
