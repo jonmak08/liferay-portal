c73e05ca26a02f868e2d41fe253264bfc2b2dd0e
Joshua Wu <joshua.wu@liferay.com>
2017-05-26 14:23:53 -0700

    updated spacing and formatting

--- modules/apps/josh-lapurd-theme/src/js/main.js
+++ modules/apps/josh-lapurd-theme/src/js/main.js
@@ -6,17 +6,20 @@ AUI().ready(
 			'scroll',
 			function() {
 				var banner = AUI.$('#banner');
-				var buttonBtt = AUI.$('.btn-back-to-top');
+				var buttonScrollTop = AUI.$('.btn-scroll-top');
+
 				var scrollY = window.pageYOffset;
 
 				if (scrollY > 350 && banner.hasClass('banner-unscrolled')) {
 					banner.toggleClass('banner-scrolled banner-unscrolled');
-					buttonBtt.fadeIn();
+
+					buttonScrollTop.fadeIn();
 				}
-					else if (scrollY <= 350 && banner.hasClass('banner-scrolled')) {
-						banner.toggleClass('banner-scrolled banner-unscrolled');
-						buttonBtt.fadeOut();
-					}
+				else if (scrollY <= 350 && banner.hasClass('banner-scrolled')) {
+					banner.toggleClass('banner-scrolled banner-unscrolled');
+
+					buttonScrollTop.fadeOut();
+				}
 			}
 		);
 
@@ -29,12 +32,13 @@ AUI().ready(
 );
 
 function toggleOpenClose() {
-	var currTarget = AUI.$(this);
-	var currTarSib = currTarget.siblings('ul.child-menu');
+	var currentTarget = AUI.$(this);
 
-	if (currTarSib !== undefined) {
-		currTarSib.toggleClass('child-menu-open');
-		currTarget.toggleClass('child-menu-toggle-open');
+	var currentTargetSibling = currentTarget.siblings('ul.child-menu');
+
+	if (currentTargetSibling) {
+		currentTargetSibling.toggleClass('child-menu-open');
+		currentTarget.toggleClass('child-menu-toggle-open');
 	}
 }
 
@@ -44,15 +48,11 @@ AUI.$('a.theme-nav-bar-toggle').click(
 	}
 );
 
-AUI.$('#childMenuDropdown_1').click(
+AUI.$('.child-menu-dropdown-1', '.child-menu-dropdown-2').click(
 	toggleOpenClose
 );
 
-AUI.$('#childMenuDropdown_2').click(
-	toggleOpenClose
-);
-
-AUI.$('.btn-back-to-top').click(
+AUI.$('.btn-scroll-top').click(
 	function() {
 		AUI.$('html, body').animate(
 			{
@@ -60,6 +60,7 @@ AUI.$('.btn-back-to-top').click(
 			},
 			700
 		);
+
 		return false;
 	}
 );
