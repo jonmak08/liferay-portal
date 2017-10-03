fe5201a95616762fec315a30e21dabdcce7d6689
Pei-Jung Lan <pei-jung.lan@liferay.com>
2017-10-02 18:34:40 -0700

    LPS-74319 Register resource command to the Group Pages portlet

--- modules/apps/foundation/mobile-device-rules/mobile-device-rules-web/src/main/java/com/liferay/mobile/device/rules/web/internal/portlet/action/RuleGroupInstancesMVCResourceCommand.java
+++ modules/apps/foundation/mobile-device-rules/mobile-device-rules-web/src/main/java/com/liferay/mobile/device/rules/web/internal/portlet/action/RuleGroupInstancesMVCResourceCommand.java
@@ -17,11 +17,17 @@ package com.liferay.mobile.device.rules.web.internal.portlet.action;
 import com.liferay.mobile.device.rules.constants.MDRPortletKeys;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCResourceCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.PortletKeys;
 
 import javax.portlet.ResourceRequest;
 import javax.portlet.ResourceResponse;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Pei-Jung Lan
@@ -30,6 +36,7 @@ import org.osgi.service.component.annotations.Component;
 	immediate = true,
 	property = {
 		"javax.portlet.name=" + MDRPortletKeys.MOBILE_DEVICE_RULES,
+		"javax.portlet.name=" + PortletKeys.GROUP_PAGES,
 		"mvc.command.name=/mobile_device_rules/rule_group_instances"
 	},
 	service = MVCResourceCommand.class
@@ -42,9 +49,24 @@ public class RuleGroupInstancesMVCResourceCommand
 			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
 		throws Exception {
 
-		include(
-			resourceRequest, resourceResponse,
-			"/layout/mobile_device_rules_rule_group_instances.jsp");
+		RequestDispatcher requestDispatcher =
+			_servletContext.getRequestDispatcher(_RULE_GROUP_INSTANCES_JSP);
+
+		requestDispatcher.include(
+			_portal.getHttpServletRequest(resourceRequest),
+			_portal.getHttpServletResponse(resourceResponse));
 	}
+
+	private static final String _RULE_GROUP_INSTANCES_JSP =
+		"/layout/mobile_device_rules_rule_group_instances.jsp";
+
+	@Reference
+	private Portal _portal;
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.mobile.device.rules.web)",
+		unbind = "-"
+	)
+	private ServletContext _servletContext;
 
 }
