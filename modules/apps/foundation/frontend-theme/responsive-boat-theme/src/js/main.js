e7ae082262ed9e363ef5074b7a90f85b8ea715a4
Alex Torres <alexander.torres@gmail.com>
2018-02-08 11:42:18 -0800

    Delete unnecessary files and update relavent ones

--- modules/apps/foundation/frontend-theme/responsive-boat-theme/src/js/main.js
+++ modules/apps/foundation/frontend-theme/responsive-boat-theme/src/js/main.js
@@ -6,39 +6,34 @@ Liferay.on('allPortletsReady', myPageJS);
 // Function Containing All of My Event Handlers
 
 function myPageJS() {
-	let nav = $('#navigation');
-	let navBtnDiv = $('#navigation div.nav-btn');
-	let portletParagraphs = $('#content .portlet-body p');
-
-	// SCROLL EVENT HANDLER
+	let nav = AUI.$('#navigation');
+	let navBtnDiv = AUI.$('#navigation div.nav-btn');
+	let portletParagraphs = AUI.$('#content .portlet-body p');
 
 	document.onscroll = function() {
-
 		// Toggle 'scrolled' class on nav based on scrolled location
-
-		if (window.scrollY >= 140)
-			nav.hasClass('scrolled') ? '' : nav.addClass('scrolled');
-		else if (nav.hasClass('scrolled'))
+		if (window.scrollY >= 140 && !nav.hasClass('scrolled'))
+			nav.addClass('scrolled');
+		else if (window.scrollY < 140 && nav.hasClass('scrolled'))
 			nav.removeClass('scrolled');
 
 		// Toggle 'visible' class to make portlet content visible when scrolled to
-
 		for (let i = 0; i < portletParagraphs.length; i++) {
 			if (!portletParagraphs.eq(i).hasClass('visible') && portletParagraphs[i].getBoundingClientRect().top - window.innerHeight <= 0)
 				portletParagraphs.eq(i).addClass('visible');
 		}
 	};
 
-	// CLICK EVENT HANDLER
-
 	navBtnDiv[0].onclick = function() {
-		let portletDiv = $('#navigation .portlet');
+		let portletDiv = AUI.$('#navigation .portlet');
 
-		// Change maximum height of nav on click to reveal or hide nav links
-		
 		nav.toggleClass('expand');
-		portletDiv.slideToggle(700, function() {
-			portletDiv.css('display', portletDiv.css('display').replace('none',''));
-		});
+
+		portletDiv.slideToggle(
+			700,
+			function() {
+				portletDiv.css('display', portletDiv.css('display').replace('none',''));
+			}
+		);
 	};
 }
