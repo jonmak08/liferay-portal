8dc5394e72e7a2513cdfeee624723e0d094ccf92
Alex Torres <alexander.torres@gmail.com>
2018-02-09 10:45:40 -0800

    Throttle scroll effect and optimize its loop

--- modules/apps/foundation/frontend-theme/responsive-boat-theme/src/js/main.js
+++ modules/apps/foundation/frontend-theme/responsive-boat-theme/src/js/main.js
@@ -6,26 +6,30 @@ function myPageJS() {
 	let navBtnDiv = nav.find('div.nav-btn');
 	let portletParagraphs = AUI.$('#content .portlet-body p');
 
-	document.onscroll = function() {
+	document.onscroll = _.throttle(function() {
 		// Toggle 'scrolled' class on nav based on scrolled location
-		let winScrollY = window.scrollY;
-
-		let hasScrolled = winScrollY < 140;
+		let hasScrolled = window.scrollY < 140;
 
 		nav.toggleClass('scrolled', !hasScrolled);
 
-		// Toggle 'visible' class to make portlet content visible when scrolled to
+		// Add 'visible' class to make portlet content visible when scrolled to
 		for (let i = 0; i < portletParagraphs.length; i++) {
-			let hasCssVisible = portletParagraphs.eq(i).hasClass('visible');
-			let scrollPositionY = portletParagraphs[i].getBoundingClientRect().top;
+			let paragraph = portletParagraphs.eq(i);
 
-			let isVisible = hasCssVisible || scrollPositionY < window.innerHeight;
+			let scrollPositionY = paragraph.get(0).getBoundingClientRect().top;
 
-			portletParagraphs.eq(i).toggleClass('visible', isVisible);
+			let isVisible = scrollPositionY < window.innerHeight;
+
+			if (isVisible) {
+				paragraph.addClass('visible');
+
+				// Remove element from jQuery object so that it's no longer tracked, and to reduce calculations
+				portletParagraphs = portletParagraphs.not(paragraph);
+			}
 		}
-	};
+	}, 200);
 
-	navBtnDiv[0].onclick = function() {
+	navBtnDiv.get(0).onclick = function() {
 		let portletDiv = nav.find('.portlet');
 
 		nav.toggleClass('expand');
