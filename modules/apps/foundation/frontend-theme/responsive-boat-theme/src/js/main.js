dc37e024d9b283c879cf7d1b70382863c661eb1e
Alex Torres <alexander.torres@gmail.com>
2018-02-08 17:36:22 -0800

    Additional revisions and optimizations

--- modules/apps/foundation/frontend-theme/responsive-boat-theme/src/js/main.js
+++ modules/apps/foundation/frontend-theme/responsive-boat-theme/src/js/main.js
@@ -1,31 +1,32 @@
-
 // This function gets loaded when everything, including the portlets, is on the page
-
 Liferay.on('allPortletsReady', myPageJS);
-
-// Function Containing All of My Event Handlers
 
 function myPageJS() {
 	let nav = AUI.$('#navigation');
-	let navBtnDiv = AUI.$('#navigation div.nav-btn');
+	let navBtnDiv = nav.find('div.nav-btn');
 	let portletParagraphs = AUI.$('#content .portlet-body p');
 
 	document.onscroll = function() {
 		// Toggle 'scrolled' class on nav based on scrolled location
-		if (window.scrollY >= 140 && !nav.hasClass('scrolled'))
-			nav.addClass('scrolled');
-		else if (window.scrollY < 140 && nav.hasClass('scrolled'))
-			nav.removeClass('scrolled');
+		let winScrollY = window.scrollY;
+
+		let hasScrolled = winScrollY < 140;
+
+		nav.toggleClass('scrolled', !hasScrolled);
 
 		// Toggle 'visible' class to make portlet content visible when scrolled to
 		for (let i = 0; i < portletParagraphs.length; i++) {
-			if (!portletParagraphs.eq(i).hasClass('visible') && portletParagraphs[i].getBoundingClientRect().top - window.innerHeight <= 0)
-				portletParagraphs.eq(i).addClass('visible');
+			let hasCssVisible = portletParagraphs.eq(i).hasClass('visible');
+			let scrollPositionY = portletParagraphs[i].getBoundingClientRect().top;
+
+			let isVisible = hasCssVisible || scrollPositionY < window.innerHeight;
+
+			portletParagraphs.eq(i).toggleClass('visible', isVisible);
 		}
 	};
 
 	navBtnDiv[0].onclick = function() {
-		let portletDiv = AUI.$('#navigation .portlet');
+		let portletDiv = nav.find('.portlet');
 
 		nav.toggleClass('expand');
 
--- modules/apps/foundation/frontend-theme/responsive-boat-theme/src/templates/navigation.ftl
+++ modules/apps/foundation/frontend-theme/responsive-boat-theme/src/templates/navigation.ftl
@@ -1,5 +1,7 @@
 <nav class="${nav_css_class}" id="navigation" role="navigation">
-	<h1 class="hide-accessible"><@liferay.language key="navigation" /></h1>
+	<h1 class="hide-accessible">
+		<@liferay.language key="navigation" />
+	</h1>
 	<div class="nav-btn icon-reorder"></div>
 	<@liferay.navigation_menu default_preferences=freeMarkerPortletPreferences.getPreferences("portletSetupPortletDecoratorId", "barebone") />
 </nav>
