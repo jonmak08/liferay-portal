862a601525656675d31b6972b76f1d307d06052f
Alex Torres <alexander.torres@gmail.com>
2018-02-22 20:10:01 -0800

    Add grandchild nav links with css and js slideToggle functions

--- modules/apps/foundation/frontend-theme/alex-rb-theme/src/js/main.js
+++ modules/apps/foundation/frontend-theme/alex-rb-theme/src/js/main.js
@@ -4,50 +4,46 @@ Liferay.on('allPortletsReady', myPageJS);
 function myPageJS() {
 	let nav = AUI.$('#navigation');
 	let navBtnDiv = nav.find('div.nav-btn');
-
-	let portletArticleElements = AUI.$('#content .journal-content-article > *');
-	let visibleArticleParents = portletArticleElements.parent().closest('.visible');
-	let visibleArticleElements = visibleArticleParents.find('.journal-content-article > *');
-
-	// Eliminate portlet article elements that are already visible
-	portletArticleElements = portletArticleElements.not(visibleArticleElements);
-
-	document.onscroll = _.throttle(
-		function() {
-			let hasScrolled = window.scrollY < 140;
-
-			// Toggle 'scrolled' class on nav and heading based on scroll location
-			nav.toggleClass('scrolled', !hasScrolled);
-
-			// Add 'visible' class to make portlet content visible when scrolled to
-			for (let i = 0; i < portletArticleElements.length; i++) {
-				let topLevelChild = portletArticleElements.eq(i);
-
-				let scrollPositionY = topLevelChild.get(0).getBoundingClientRect().top;
-
-				let hasClassVisible = scrollPositionY < window.innerHeight;
-
-				if (hasClassVisible) {
-					topLevelChild.addClass('visible');
-
-					// Remove element from jQuery object so that it's no longer tracked, and to reduce calculations
-					portletArticleElements = portletArticleElements.not(topLevelChild);
-				}
-			}
-		},
-		200
-	);
+	let parentDropdownBtn = nav.find('> ul > li > span.icon-angle-down');
+	let childDropdownBtns = nav.find('.child-menu span.icon-angle-down');
 
 	navBtnDiv.get(0).onclick = function() {
-		let portletDiv = nav.find('.portlet');
+		let navUl = nav.find('.nav');
 
 		$(this).toggleClass('expand');
 
-		portletDiv.slideToggle(
+		navUl.slideToggle(
 			300,
 			function() {
-				portletDiv.css('display', portletDiv.css('display').replace('none', ''));
+				$(this).css('display', $(this).css('display').replace('none', ''));
 			}
 		);
+	};
+
+	function dropdownClickHandler(target) {
+		let siblingUl = $(target).find('+ ul');
+
+		$(target).toggleClass('expand');
+
+		siblingUl.slideToggle(
+			300,
+			function() {
+				$(this).css('display', $(this).css('display').replace('none', ''));
+			}
+		);
+	}
+
+	childDropdownBtns.each(
+		function() {
+			this.onclick = function() {
+				dropdownClickHandler(this);
+			};
+		}
+	);
+
+	parentDropdownBtn.get(0).onclick = function() {
+		if (window.innerWidth < 768) {
+			dropdownClickHandler(this);
+		}
 	};
 }
